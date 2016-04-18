c21b636bac140eed9137881565a4d6d9c60cf1a6
Edward Bustamante Acevedo <edwabuac@suramericana.com.co>
2016-04-18 11:12:00 -0500

    Deuda Tecnica Se cambia el import org.openqa.selenium.*;  por import org.openqa.selenium.keys; para no cargar toda la libreria

--- src/main/java/com/sura/policycenter/selenium/pages/Escritorio.java
+++ src/main/java/com/sura/policycenter/selenium/pages/Escritorio.java
@@ -3,9 +3,11 @@ package com.sura.policycenter.selenium.pages;
 import com.sura.guidewire.selenium.Guidewire;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.WhenPageOpens;
-import org.openqa.selenium.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
@@ -57,8 +59,8 @@ public class Escritorio extends Guidewire {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
         }
+
     }
 
 }
