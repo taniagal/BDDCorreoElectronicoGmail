59303ed4b2b80950084027c92824fae6eca30c0a
Carlos Acevedo <ceacevedo@sura.com.co>
2016-04-21 16:01:52 -0500

    Login SEUS
    Se creo la prueba para el login en PolicyCenter usando SEUS

--- src/main/java/com/sura/policycenter/selenium/steps/PolicySteps.java
+++ src/main/java/com/sura/policycenter/selenium/steps/PolicySteps.java
@@ -1,5 +1,6 @@
 package com.sura.policycenter.selenium.steps;
 
+import com.sura.guidewire.selenium.SeusLoginPage;
 import com.sura.policycenter.selenium.pages.Escritorio;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.pages.Pages;
@@ -7,14 +8,12 @@ import net.thucydides.core.steps.ScenarioSteps;
 import com.sura.guidewire.selenium.Guidewire;
 import com.sura.policycenter.selenium.pages.AbrirApp;
 
-/**
- * Created by jorghome on 16/03/2016.
- */
 public class PolicySteps extends ScenarioSteps {
 
+    SeusLoginPage seus = new SeusLoginPage(getDriver());
     Guidewire gw = new Guidewire(getDriver());
     AbrirApp abrirapp = new AbrirApp(getDriver());
-    Escritorio escritorio = new Escritorio(getDriver());
+    Escritorio escritorioPage = new Escritorio(getDriver());
 
     public PolicySteps(Pages pages) {
         super(pages);
@@ -26,18 +25,28 @@ public class PolicySteps extends ScenarioSteps {
     }
 
     @Step
-    public void login(String usuario, String contrasena) {
-        gw.login(usuario, contrasena);
+    public void openSeus() {
+        seus.open();
+    }
+
+    @Step
+    public void login(String usuario, String contrasenia) {
+        gw.login(usuario, contrasenia);
+    }
+
+    @Step
+    public void loginSeus(String pais,String usuario, String contrasenia) {
+        seus.login(pais, usuario, contrasenia);
     }
 
     @Step
     public void assertion(String element) {
-        escritorio.assertion(element);
+        escritorioPage.assertion(element);
     }
 
     @Step
     public void nuevoContactoPersona() {
-        escritorio.nuevoContactoPersona();
+        escritorioPage.nuevoContactoPersona();
     }
 
     @Step
