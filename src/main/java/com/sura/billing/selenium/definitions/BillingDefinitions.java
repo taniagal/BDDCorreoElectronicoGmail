4021d0c69e90ac356ae5d0d27d5cb67d602b55c4
Jorge Andres Holguin Medina <jorghome@suramericana.com.co>
2016-04-25 09:30:00 -0500

    test

--- src/main/java/com/sura/billing/selenium/definitions/BillingDefinitions.java
+++ src/main/java/com/sura/billing/selenium/definitions/BillingDefinitions.java
@@ -1,7 +1,42 @@
 package com.sura.billing.selenium.definitions;
 
+import com.sura.billing.selenium.steps.BillingSteps;
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
 /**
  * Created by jorghome on 22/04/2016.
  */
 public class BillingDefinitions {
+
+    @Steps
+    BillingSteps bcs;
+
+    @Given("estoy en la pagina de autenticacion de BillingCenter")
+    public void open() {
+        bcs.open();
+    }
+
+    @When("ingrese username <usr> y password <pass>")
+    public void login(@Named("usr") String user, @Named("pass") String password) {
+        bcs.login(user, password);
+    }
+
+    @Then("deberia ingresar al sistema y ver el mensaje <message>")
+    public void assertion(@Named("message") String msjResult) {
+        bcs.assertion(msjResult);
+    }
+
+    @Then("el usuario deberia poder acceder a la administracion de dias festivos")
+    public void admDiasFestivos() {
+        bcs.admDiasFestivos();
+    }
+
+    @Then("el usuario deberia cerrar sesion")
+    public void logout() {
+        bcs.logout();
+    }
 }
--- src/main/java/com/sura/billing/selenium/pages/AbrirAppPage.java
+++ src/main/java/com/sura/billing/selenium/pages/AbrirAppPage.java
@@ -1,7 +1,16 @@
 package com.sura.billing.selenium.pages;
 
+import com.sura.guidewire.selenium.Guidewire;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by jorghome on 22/04/2016.
  */
-public class AbrirAppPage {
+// TODO: 22/04/2016 URL temporal mientras se despliega en los otros ambientes
+@DefaultUrl("http://10.209.16.122:8580/bc/BillingCenter.do")
+public class AbrirAppPage extends Guidewire {
+    public AbrirAppPage(WebDriver driver) {
+        super(driver);
+    }
 }
