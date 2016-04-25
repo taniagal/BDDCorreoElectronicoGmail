b2e97be60be253b0a929b36767f748c0bc464af5
Carlos Acevedo <ceacevedo@sura.com.co>
2016-04-25 16:27:30 -0500

    Escenario buscar cuenta asociada a un contacto
    Queda faltando realizar un refactor a los test

--- src/main/java/com/sura/policycenter/selenium/definitions/ContactAssociatedAccountDefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/ContactAssociatedAccountDefinitions.java
@@ -1,15 +1,31 @@
 package com.sura.policycenter.selenium.definitions;
 
 
+import com.sura.guidewire.selenium.SeusLoginSteps;
+import com.sura.policycenter.selenium.steps.ContactAssociatedAccountSteps;
+import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
+import javax.swing.*;
+
 public class ContactAssociatedAccountDefinitions {
 
+    @Steps
+    SeusLoginSteps seusSteps;
+
+    @Steps
+    ContactAssociatedAccountSteps contactSteps;
+
+
+
     @Given("que me encuentro autenticado con pais <pais>, username <usr> y password <pass>")
-    public void givenQueMeEncuentroAutenticadoConPaispaisUsernameusrYPasswordpass() {
-        // PENDING
+    public void login(@Named("pais") String pais,
+                      @Named("usr") String usuario,
+                      @Named("pass") String password) {
+        seusSteps.login(pais, usuario, password);
     }
 
     @Given("existe una cuenta asociada a un contacto")
@@ -19,11 +35,15 @@ public class ContactAssociatedAccountDefinitions {
 
     @When("consulte las cuentas asociadas a un contacto")
     public void whenConsulteLasCuentasAsociadasAUnContacto() {
-        // PENDING
+        contactSteps.searchContact();
     }
 
     @Then("se muestra la lista de cuentas asociadas con nombre: <nombre>, direccion: <direccion>, tel: <telefono>, email: <email> y rol: <rol>")
-    public void thenSeMuestraLaListaDeCuentasAsociadasConNombrenombreDirecciondireccionTeltelefonoEmailemailYRolrol() {
-        // PENDING
+    public void searchAssociatedAccounts(@Named("nombre") String nombre,
+                                         @Named("direccion") String direccion,
+                                         @Named("telefono") String telefono,
+                                         @Named("email") String email,
+                                         @Named("rol") String rol) {
+        contactSteps.searchAssociatedAccounts(nombre, direccion, telefono, email, rol);
     }
 }
---
+++ src/main/java/com/sura/policycenter/selenium/pages/ContactAssociatedAccount.java
@@ -0,0 +1,45 @@
+package com.sura.policycenter.selenium.pages;
+
+import com.sura.guidewire.selenium.SeusLoginPage;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
+public class ContactAssociatedAccount extends SeusLoginPage {
+
+    @FindBy(xpath="//td[@id='ContactFile:MenuLinks:ContactFile_ContactFile_Accounts']/div")
+    WebElementFacade mnuLateralCuenta;
+
+    @FindBy(xpath="//div/table/tbody/tr/td[2]/div")
+    WebElementFacade nombreContactoCuenta;
+
+    @FindBy(xpath="//td[3]/div")
+    WebElementFacade direccionContactoCuenta;
+
+    @FindBy(xpath="//td[4]/div")
+    WebElementFacade telContactoCuenta;
+
+    @FindBy(xpath="//td[5]/div")
+    WebElementFacade emailContactoCuenta;
+
+    @FindBy(xpath="//td[6]/div")
+    WebElementFacade rolesContactoCuenta;
+
+    public ContactAssociatedAccount(WebDriver driver) {
+        super(driver);
+    }
+
+    public void assertCamposLista(String nombre, String direccion,
+                                  String telefono, String email, String rol){
+        mnuLateralCuenta.click();
+        assertThat(nombreContactoCuenta.getText().toString(), containsString(nombre));
+        assertThat(direccionContactoCuenta.getText().toString(), containsString(direccion));
+        assertThat(telContactoCuenta.getText().toString(), containsString(telefono));
+        assertThat(emailContactoCuenta.getText().toString(), containsString(email));
+        assertThat(rolesContactoCuenta.getText().toString(), containsString(rol));
+    }
+
+}
