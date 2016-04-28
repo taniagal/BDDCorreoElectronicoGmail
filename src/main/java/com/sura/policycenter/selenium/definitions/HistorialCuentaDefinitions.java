a84ee0c51f5094083afb7279b548e6cb8ae71718
jhonvebz <jhon.vergara@ceiba.com.co>
2016-04-28 10:36:04 -0500

    Se quita pending

--- src/main/java/com/sura/policycenter/selenium/definitions/HistorialCuentaDefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/HistorialCuentaDefinitions.java
@@ -68,51 +68,35 @@ public class HistorialCuentaDefinitions {
 
     @When("se realiza la busqueda por <fecha> y solo se indique la opcion desde")
     @Manual
-    public void whenSeRealizaLaBusquedaPorfechaYSoloSeIndiqueLaOpcionDesde(@Named("fecha") String fechaDesde) {
-        // PENDING
-    }
+    public void whenSeRealizaLaBusquedaPorfechaYSoloSeIndiqueLaOpcionDesde(@Named("fecha") String fechaDesde) {}
 
     @Then("debo poder ver las transacciones en el rango de fechas seleccionado, teniendo en cuenta que la opcion hasta corresponde a la fecha actual.")
     @Manual
-    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionadoTeniendoEnCuentaQueLaOpcionHastaCorrespondeALaFechaActual() {
-        // PENDING
-    }
+    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionadoTeniendoEnCuentaQueLaOpcionHastaCorrespondeALaFechaActual() {}
 
     @When("se realiza la busqueda por <fecha> y solo se indique la opcion hasta")
     @Manual
-    public void whenSeRealizaLaBusquedaPorfechaYSoloSeIndiqueLaOpcionHasta(@Named("fecha") String fechaHasta) {
-        // PENDING
-    }
+    public void whenSeRealizaLaBusquedaPorfechaYSoloSeIndiqueLaOpcionHasta(@Named("fecha") String fechaHasta) {}
 
     @Then("debo poder ver las transacciones en el rango de fechas seleccionado, teniendo en cuenta que la opcion desde corresponde a la fecha de inicio de la cuenta")
     @Manual
-    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionadoTeniendoEnCuentaQueLaOpcionDesdeCorrespondeALaFechaDeInicioDeLaCuenta() {
-        // PENDING
-    }
+    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionadoTeniendoEnCuentaQueLaOpcionDesdeCorrespondeALaFechaDeInicioDeLaCuenta() {}
 
     @When("se realiza la busqueda por fecha ( <desde> - <hasta>)")
     @Manual
-    public void whenSeRealizaLaBusquedaPorFechadesdehasta() {
-        // PENDING
-    }
+    public void whenSeRealizaLaBusquedaPorFechadesdehasta() {}
 
     @Then("debo poder ver las transacciones en el rango de fechas seleccionado.")
     @Manual
-    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionado() {
-        // PENDING
-    }
+    public void thenDeboPoderVerLasTransaccionesEnElRangoDeFechasSeleccionado() {}
 
     @When("indique un <usuario> para realizar la busqueda")
     @Manual
-    public void whenIndiqueUnusuarioParaRealizarLaBusqueda() {
-        // PENDING
-    }
+    public void whenIndiqueUnusuarioParaRealizarLaBusqueda() {}
 
     @Then("debo poder ver las transacciones asociadas a ese usuario especifico, que estan relacionadas con la cuenta y las polizas asociadas a ella.")
     @Manual
-    public void thenDeboPoderVerLasTransaccionesAsociadasAEseUsuarioEspecificoQueEstanRelacionadasConLaCuentaYLasPolizasAsociadasAElla() {
-        // PENDING
-    }
+    public void thenDeboPoderVerLasTransaccionesAsociadasAEseUsuarioEspecificoQueEstanRelacionadasConLaCuentaYLasPolizasAsociadasAElla() {}
 
     @When("indique el criterio de busqueda Relacionado con")
     public void whenIndiqueElCriterioDeBusquedaRelacionadoCon() {
@@ -152,43 +136,29 @@ public class HistorialCuentaDefinitions {
 
     @When("seleccione  el  numero de la <poliza>")
     @Manual
-    public void whenSeleccioneElNumeroDeLapoliza() {
-        // PENDING
-    }
+    public void whenSeleccioneElNumeroDeLapoliza() {}
 
     @Then("debo poder ver el campo poliza habilitado como un vinculo para el detalle de la transaccion")
     @Manual
-    public void thenDeboPoderVerElCampoPolizaHabilitadoComoUnVinculoParaElDetalleDeLaTransaccion() {
-        // PENDING
-    }
+    public void thenDeboPoderVerElCampoPolizaHabilitadoComoUnVinculoParaElDetalleDeLaTransaccion() {}
 
     @When("seleccione  el  numero de la transaccion <numeroTransaccion> de la poliza")
     @Manual
-    public void whenSeleccioneElNumeroDeLaTransaccionnumerotransaccionDeLaPoliza() {
-        //PENDING
-    }
+    public void whenSeleccioneElNumeroDeLaTransaccionnumerotransaccionDeLaPoliza() {}
 
     @Then("debo poder ver el detalle de la transaccion")
     @Manual
-    public void thenDeboPoderVerElDetalleDeLaTransaccion() {
-        //PENDING
-    }
+    public void thenDeboPoderVerElDetalleDeLaTransaccion() {}
 
     @When("se realice una modificacion valorable a la <poliza> de la cuenta")
     @Manual
-    public void whenSeRealiceUnaModificacionValorableALapolizaDeLaCuenta() {
-        // PENDING
-    }
+    public void whenSeRealiceUnaModificacionValorableALapolizaDeLaCuenta() {}
 
     @Then("poder ver el <valorOriginal> de la poliza antes de realizar el cambio")
     @Manual
-    public void thenPoderVerElvalororiginalDeLaPolizaAntesDeRealizarElCambio() {
-        //PENDING
-    }
+    public void thenPoderVerElvalororiginalDeLaPolizaAntesDeRealizarElCambio() {}
 
     @Then("poder ver el <valorNuevo> de la poliza despues de realizar el cambio")
     @Manual
-    public void thenPoderVerElvalornuevoDeLaPolizaDespuesDeRealizarElCambio() {
-        //PENDING
-    }
+    public void thenPoderVerElvalornuevoDeLaPolizaDespuesDeRealizarElCambio() {}
 }
