80b2191e1d3ea5a4aed17e77165778e662377b49
Jorge Alberto Santacruz Perez <jorgsape@suramericana.com.co>
2016-05-03 11:38:39 -0500

    Refactor
    Eliminar Threads

--- src/main/java/com/sura/policycenter/selenium/pages/NuevoContactoPage.java
+++ src/main/java/com/sura/policycenter/selenium/pages/NuevoContactoPage.java
@@ -6,7 +6,9 @@ import com.sura.policycenter.constantes.EnumContacto;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
-import org.junit.Assert;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.openqa.selenium.WebDriver;
 
 
@@ -17,17 +19,17 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * Created by brayruru on 2016/04/27.
  */
-public class NuevoContactoPage extends Guidewire{
+public class NuevoContactoPage extends Guidewire {
 
 
     public NuevoContactoPage(WebDriver driver) {
         super(driver);
     }
-
 
 
     @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:OfficialIDInputSet:DocumentType-inputEl']")
@@ -49,89 +51,88 @@ public class NuevoContactoPage extends Guidewire{
     @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:AddressLine1-inputEl']")
     private WebElementFacade direccion;
 
-    @FindBy(xpath =".//*[@id='NewContact:Update-btnInnerEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:Update-btnInnerEl']")
     private WebElementFacade actualizar;
 
-    @FindBy(xpath =".//*[@id='ContactFile_Details:ContactFile_DetailsInternalScreen:InternalDetailsCardPanelCV:AccountContactDV:ContactNameInputSet:GlobalPersonNameInputSet:FirstName-inputEl']")
+    @FindBy(xpath = ".//*[@id='ContactFile_Details:ContactFile_DetailsInternalScreen:InternalDetailsCardPanelCV:AccountContactDV:ContactNameInputSet:GlobalPersonNameInputSet:FirstName-inputEl']")
     private WebElementFacade nombreContact;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:GlobalContactNameInputSet:Name-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:GlobalContactNameInputSet:Name-inputEl']")
     private WebElementFacade razonSocial;
 
-    @FindBy(xpath =".//*[@id='ContactFile_Details:ContactFile_DetailsInternalScreen:InternalDetailsCardPanelCV:AccountContactDV:ContactNameInputSet:GlobalContactNameInputSet:Name-inputEl']")
+    @FindBy(xpath = ".//*[@id='ContactFile_Details:ContactFile_DetailsInternalScreen:InternalDetailsCardPanelCV:AccountContactDV:ContactNameInputSet:GlobalContactNameInputSet:Name-inputEl']")
     private WebElementFacade desRazonSocial;
 
-    @FindBy(xpath =".//*[@id='NewContact:_msgs']/div")
+    @FindBy(xpath = ".//*[@id='NewContact:_msgs']/div")
     private WebElementFacade contactoExistente;
-	
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:PersonDataContact:CreateNewContactInputSet:PrimaryPhone-inputEl']")
+
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:PersonDataContact:CreateNewContactInputSet:PrimaryPhone-inputEl']")
     private WebElementFacade tipoTelefono;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:WorkPhone:GlobalPhoneInputSet:NationalSubscriberNumber-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:WorkPhone:GlobalPhoneInputSet:NationalSubscriberNumber-inputEl']")
     private WebElementFacade telefonoTrabajo;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:HomePhone:GlobalPhoneInputSet:NationalSubscriberNumber-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:ContactNameInputSet:HomePhone:GlobalPhoneInputSet:NationalSubscriberNumber-inputEl']")
     private WebElementFacade telefonoResidencia;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:Country-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:Country-inputEl']")
     private WebElementFacade pais;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:State-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:State-inputEl']")
     private WebElementFacade departamento;
 
-    @FindBy(xpath =".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:City-inputEl']")
+    @FindBy(xpath = ".//*[@id='NewContact:ContactPanelSet:ContactCV:ContactDV:AddressInputSet:globalAddressContainer:GlobalAddressInputSet:City-inputEl']")
     private WebElementFacade ciudad;
 
-   // @FindBy( xpath =".//*[@id='NewContact:_msgs']")
     private WebElementFacade arregloDeMensajes;
 
 
-    public void seleccionarTipoDocumento (String tipoDocumento){
-     this.tipoDocumento.type(tipoDocumento);
-        setImplicitTimeout(5, SECONDS);
+    public void seleccionarTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento.type(tipoDocumento);
+        setImplicitTimeout(1, SECONDS);
         this.tipoDocumento.click();
     }
 
-    public void ingresarNumeroDocumento( String numeroDocumento){
+    public void ingresarNumeroDocumento(String numeroDocumento) {
         this.numeroDocumento.type(numeroDocumento);
     }
 
-    public void ingresarPrimerNombre( String primerNombre){
+    public void ingresarPrimerNombre(String primerNombre) {
         this.primerNombre.type(primerNombre);
     }
 
-    public void ingresarPrimerApellido( String primerApellido){
+    public void ingresarPrimerApellido(String primerApellido) {
         this.primerApellido.type(primerApellido);
     }
 
-    public void seleccionarTipoDireccion(String tipoDireccion){
+    public void seleccionarTipoDireccion(String tipoDireccion) {
         this.tipoDireccion.type(tipoDireccion);
-        setImplicitTimeout(5, SECONDS);
+        setImplicitTimeout(1, SECONDS);
         this.tipoDireccion.click();
     }
 
-    public void ingresarDireccion(String direccion){
+    public void ingresarDireccion(String direccion) {
         this.direccion.type(direccion);
         this.direccion.click();
     }
 
-    public void actualizarPersonaNatural(){
+    public void actualizarPersonaNatural() {
         wait_for_the_element_to_be_clickable(this.actualizar);
         this.actualizar.click();
 
-        setImplicitTimeout(5, SECONDS);
-        Assert.assertEquals(this.nombreContact.getText().toString(), "BRAYAN");
+        setImplicitTimeout(1, SECONDS);
+        assertThat(this.nombreContact.getText().toString(), containsString("BRAYAN"));
 
     }
 
-    public void actualizar(){
+    public void actualizar() {
         wait_for_the_element_to_be_clickable(this.actualizar);
         this.actualizar.click();
-        setImplicitTimeout(5, SECONDS);
+        setImplicitTimeout(1, SECONDS);
 
     }
 
-    public void ingresarTelefonoFijo(String tipoTelefono, String numeroTelefono){
+    public void ingresarTelefonoFijo(String tipoTelefono, String numeroTelefono) {
         if (esTelefonoFijo(tipoTelefono)) {
             ingresarTelefono(tipoTelefono, numeroTelefono);
         }
@@ -142,12 +143,12 @@ public class NuevoContactoPage extends Guidewire{
 
         if ("Trabajo".equals(tipoTelefono)) {
             this.tipoTelefono.type(tipoTelefono);
-            setImplicitTimeout(2, SECONDS);
+            setImplicitTimeout(1, SECONDS);
             this.tipoTelefono.click();
             this.telefonoTrabajo.type(numeroTelefono);
         } else if ("Vivienda".equals(tipoTelefono)) {
             this.tipoTelefono.type(tipoTelefono);
-            setImplicitTimeout(2, SECONDS);
+            setImplicitTimeout(1, SECONDS);
             this.tipoTelefono.click();
             this.telefonoResidencia.type(numeroTelefono);
         } else {
@@ -159,36 +160,29 @@ public class NuevoContactoPage extends Guidewire{
 
 
     //ESCENARIO NUEVO CONTACTO PERSONA JURIDICA
-    public void ingresarRazonSocial( String razonSocial){
+    public void ingresarRazonSocial(String razonSocial) {
         this.razonSocial.type(razonSocial);
     }
 
     public void actualizarJuridica() {
-        try {
-            wait_for_the_element_to_be_clickable(this.actualizar);
-            this.actualizar.click();
-            Thread.sleep(3000);
-            Assert.assertEquals(this.desRazonSocial.getText().toString(), "La Vaquita");
-        } catch (InterruptedException e) {
 
-            throw new RuntimeException(e);
-        }
+        wait_for_the_element_to_be_clickable(this.actualizar);
+        this.actualizar.click();
+        setImplicitTimeout(1, SECONDS);
+        assertThat(this.desRazonSocial.getText().toString(), containsString("La Vaquita"));
+
     }
 
     public void verificarContactoExistente() {
-        try {
-            wait_for_the_element_to_be_clickable(this.actualizar);
-            this.actualizar.click();
-            Thread.sleep(3000);
-            Assert.assertEquals(this.contactoExistente.getText().toString(), "Ya existe un contacto con el mismo número de identificación");
-        } catch (InterruptedException e) {
 
-            throw new RuntimeException(e);
-        }
+        wait_for_the_element_to_be_clickable(this.actualizar);
+        this.actualizar.click();
+        setImplicitTimeout(1, SECONDS);
+        assertThat(this.contactoExistente.getText().toString(), containsString("Ya existe un contacto con el mismo número de identificación"));
     }
 
-    public Boolean esTelefonoFijo(String tipoTelefono){
-        if ("Trabajo".equals(tipoTelefono) || "Vivienda".equals(tipoTelefono)){
+    public Boolean esTelefonoFijo(String tipoTelefono) {
+        if ("Trabajo".equals(tipoTelefono) || "Vivienda".equals(tipoTelefono)) {
             return Boolean.TRUE;
         }
 
@@ -203,34 +197,34 @@ public class NuevoContactoPage extends Guidewire{
 
     //ESCENARIO CAMPO PAIS, DEPARTAMENTO Y CIUDAD OBLIGATORIO
 
-    public void validarCampoPaisDepartamentoYCiudad(){
+    public void validarCampoPaisDepartamentoYCiudad() {
 
         if (esTelefonoFijo(this.tipoTelefono.getValue())) {
             actualizar();
 
-            List<WebElementFacade> tabs = withTimeoutOf(5, SECONDS).findAll(".//*[@id='NewContact:_msgs']//div");
+            List<WebElementFacade> tabs = withTimeoutOf(1, SECONDS).findAll(".//*[@id='NewContact:_msgs']//div");
 
             EnumContacto mensajeRequerido = null;
             boolean flag = false;
 
-            for (WebElementFacade div: tabs) {
+            for (WebElementFacade div : tabs) {
                 String textoRequerido = div.getText();
                 flag = false;
 
-                if (textoRequerido.toLowerCase().contains(EnumContacto.CIUDAD.name().toLowerCase())){
+                if (textoRequerido.toLowerCase().contains(EnumContacto.CIUDAD.name().toLowerCase())) {
 
                     mensajeRequerido = EnumContacto.CIUDAD;
-                    flag=true;
+                    flag = true;
                 }
 
-                if (textoRequerido.toLowerCase().contains(EnumContacto.DEPARTAMENTO.name().toLowerCase())){
+                if (textoRequerido.toLowerCase().contains(EnumContacto.DEPARTAMENTO.name().toLowerCase())) {
 
                     mensajeRequerido = EnumContacto.DEPARTAMENTO;
-                    flag=true;
+                    flag = true;
                 }
 
-                if(flag) {
-                    Assert.assertEquals(textoRequerido, mensajeRequerido.getMensajeRequerido());
+                if (flag) {
+                    assertThat(textoRequerido, containsString(mensajeRequerido.getMensajeRequerido()));
                 }
 
             }
