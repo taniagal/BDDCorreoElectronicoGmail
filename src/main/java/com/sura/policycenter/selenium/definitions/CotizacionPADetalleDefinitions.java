613398df87745ed30a61be0397344b49884e64a2
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-06-08 13:57:22 -0500

    Validación campos detalle cotización

--- src/main/java/com/sura/policycenter/selenium/definitions/CotizacionPADetalleDefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/CotizacionPADetalleDefinitions.java
@@ -1,5 +1,6 @@
 package com.sura.policycenter.selenium.definitions;
 
+import com.sura.policycenter.selenium.pages.InicioPage;
 import com.sura.policycenter.selenium.steps.CotizacionPADetalleSteps;
 import com.sura.policycenter.selenium.steps.HistorialCuentaSteps;
 import net.thucydides.core.annotations.Steps;
@@ -10,10 +11,19 @@ import org.jbehave.core.annotations.Then;
 import java.util.HashMap;
 import java.util.Map;
 
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getPages;
+
 public class CotizacionPADetalleDefinitions {
 
     @Steps
     CotizacionPADetalleSteps cotizacionPADetalleSteps;
+
+    @Steps
+    HistorialCuentaSteps historialCuentaSteps;
+
+    private InicioPage inicioPage() {
+        return getPages().currentPageAt(InicioPage.class);
+    }
 
     private final Map<String, String> infoDetalleCotizacion = new HashMap<>();
 
@@ -32,12 +42,9 @@ public class CotizacionPADetalleDefinitions {
         infoDetalleCotizacion.put("impuesto", "Impuesto");
     }
 
-    @Steps
-    HistorialCuentaSteps historialCuentaSteps;
-
-    @Given("estoy en una cuenta <numeroCuenta>")
-    public void givenEstoyEnUnaCuentanumCuenta(@Named("numCuenta") String numCuenta) {
-        historialCuentaSteps.seleccionarCuenta(numCuenta);
+    @Given("he realizado la cotizacion <cotizacion>")
+    public void irALaCotizacion(@Named("cotizacion") String cotizacion) {
+        //inicioPage().irABuscarSubPoliza(cotizacion);
     }
 
     @Then("se debe mostrar la informacion del detalle de cotizacion por riesgo")
