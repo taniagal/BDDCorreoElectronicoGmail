c45d1c0b039393f2ce4e56ea3ed6848980232fb5
Jonathan Mejia <jmejia@qvision.com.co>
2016-06-22 08:34:44 -0500

    Refinamiento y finalización Declinar
    Se realiza BDD con 3 escenarios que cubren las pruebas de aceptacion

--- src/main/java/com/sura/policycenter/selenium/definitions/AdmonCotizacionCuentaDefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/AdmonCotizacionCuentaDefinitions.java
@@ -1,33 +1,60 @@
 package com.sura.policycenter.selenium.definitions;
 
+import com.sura.policycenter.selenium.steps.AdmonCotizacionCuentaSteps;
+import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
 
 
 public class AdmonCotizacionCuentaDefinitions {
 
-
-    
-
+    @Steps
+    AdmonCotizacionCuentaSteps admonCotizacionCuentaSteps;
 
     @Given("que estoy visualizando las cotizaciones de la cuenta <numCuenta>")
-    public void visualizaCotizacion() {
-
+    public void visualizaCotizacion(@Named("numCuenta") String numCuenta) {
+        admonCotizacionCuentaSteps.navegar_barra_superior(numCuenta);
     }
 
-    @When("quiera declinar la cotizacion por cualquiera de la razones")
-    public void razonDeclinarCotizacion() {
-        // PENDING
+    @When("quiera declinar la cotizacion por cualquier razon <razon>")
+    public void razonDeclinarCotizacion(@Named("razon") String razon) {
+        admonCotizacionCuentaSteps.seleccionar_opcion_en_acciones();
+        admonCotizacionCuentaSteps.ingresar_informacion_al_momento_de_rechazar(razon);
     }
 
-    @Then("el sistema debe mostrar el estado declinada")
-    public void estadoDeclinarCotizacion() {
-        // PENDING
+    @Then("el sistema debe mostrar el estado declinada <numCotizacion>")
+    public void estadoDeclinarCotizacion(@Named("numCotizacion") String numCotizacion) {
+        admonCotizacionCuentaSteps.valida_estado_de_la_cotizacion(numCotizacion);
     }
 
     @Then("desahabilitar la opcion de acciones para no ser modificada")
     public void thenDesahabilitarLaOpcionDeAccionesParaNoSerModificada() {
-        // PENDING
+        admonCotizacionCuentaSteps.inhabilita_acciones();
     }
+
+    @When("quiera declinar la cotizacion sin ingresar la razon")
+    public void navegaCotizacion() {
+        admonCotizacionCuentaSteps.seleccionar_opcion_en_acciones();
+        admonCotizacionCuentaSteps.declinar_sin_razon();
+    }
+
+    @Then("el sistema debe mostrar un mesaje <mensaje>")
+    public void mostarMensaje(@Named("mensaje") String mensaje) {
+        admonCotizacionCuentaSteps.valida_mensaje(mensaje);
+    }
+
+    @When("quiera declinar la cotizacion")
+    public void declinar () {
+        admonCotizacionCuentaSteps.seleccionar_opcion_en_acciones();
+    }
+
+    @Then("el sistema debe mostrarme todas las opciones: $listaCodRazon")
+    public void validarOpciones(ExamplesTable listaCodRazon) throws Exception {
+        admonCotizacionCuentaSteps.valida_lista_de_razones(listaCodRazon);
+
+    }
+
 }
