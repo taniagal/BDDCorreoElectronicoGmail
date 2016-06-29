d1e2c42710a91057e7cd834d6012d905fcbcae0e
Jonathan Mejia <jmejia@qvision.com.co>
2016-06-29 15:03:14 -0500

    Clase Definitios Terminada
    Se realizaron las storys y la clase definitios

---
+++ src/main/java/com/sura/policycenter/selenium/definitions/InformacionDePolizaMrcDefinitions.java
@@ -0,0 +1,61 @@
+package com.sura.policycenter.selenium.definitions;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class InformacionDePolizaMrcDefinitions {
+
+
+    @Given("se inicio una nueva suscripcion <numeroCuenta>")
+    public void iniciaNuevaSuscripcionCuenta(@Named("numeroCuenta")String numeroCuenta) {
+        // PENDING
+    }
+
+    @When("este expidiendo una poliza de propiedad comercial")
+    public void expidePolizaPropiedadComercial() {
+        // PENDING
+    }
+
+    @Then("se debe visualizar los datos del tomador y tomador adicional completos")
+    public void datosDelTomadorYAdicional() {
+        // PENDING
+    }
+
+    /*
+    * 2 Historia
+    */
+
+    @When("modifique la fecha de inicio de vigencia de la poliza <fechaInicioVigencia>")
+    public void modificaFechaInicioFechaFin(@Named("fechaInicioVigencia")String fechaInicioVigencia) {
+        // PENDING
+    }
+
+    @Then("se debe mostrar un mensaje <mensaje> de error")
+    public void mostarMensaError(@Named("mensaje")String mensaje) {
+        // PENDING
+    }
+
+    @Then("bloquear la operacion")
+    public void bloqueoDeAplicacion() {
+        // PENDING
+    }
+
+    /*
+    * 3 Historia
+    */
+
+    @When("quiera agregar un tomador adicional que es riesgo consultable <cedula>")
+    public void tomadorAdicionalRiesgoConsultabale(@Named("cedula")String cedula) {
+
+    }
+
+    @Then("se debe mostrar un mensaje de error <mensaje> <mensaje2>")
+    public void mensajesDeErrorRiesgosYPeps(@Named("mensaje")String mensaje,
+                                            @Named("mensaje2")String mensaje2) {
+
+    }
+
+
+}
---
+++ src/main/java/com/sura/policycenter/selenium/steps/InformacionDePolizaMrcSteps.java
@@ -0,0 +1,7 @@
+package com.sura.policycenter.selenium.steps;
+
+/**
+ * Created by jonamele on 29/06/2016.
+ */
+public class InformacionDePolizaMrcSteps {
+}
