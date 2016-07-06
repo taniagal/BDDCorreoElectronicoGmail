8495b90980f45690a31230732e0a2d212d650c7a
Eliana Alexandra Alvarez Chaverra <eliaalch@suramericana.com.co>
2016-07-06 16:49:31 -0500

    Disponibilidad Detalle producto
    -estabilizacion de pruebas para subir a jenkins

--- src/main/java/com/sura/policycenter/selenium/pages/DisponibilidadDetalleProductoPage.java
+++ src/main/java/com/sura/policycenter/selenium/pages/DisponibilidadDetalleProductoPage.java
@@ -1,5 +1,6 @@
 package com.sura.policycenter.selenium.pages;
 
+import com.google.common.base.Function;
 import com.sura.guidewire.selenium.Guidewire;
 import com.sura.serinitybdd.util.GwNavegacionUtil;
 import java.util.List;
@@ -11,10 +12,12 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 
 public class DisponibilidadDetalleProductoPage extends Guidewire {
@@ -45,30 +48,32 @@ public class DisponibilidadDetalleProductoPage extends Guidewire {
     }
 
     public void accionarNuevoEnvio() {
-        btnAccionesCuenta.waitUntilPresent().click();
+        btnAccionesCuenta.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().click();
 //        btnAccionesCuenta.click();
-        opcionNuevoEnvio.waitUntilPresent().click();
-        waitABit(1000);
+        opcionNuevoEnvio.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().click();
+//        waitABit(1000);
         this.irAVerInformacionDePoliza();
 //        btnElegirAutoPersonal.click();
     }
 
     public void verInformacionDePoliza() {
-        waitABit(1000);
-        opcionVerInformacionPoliza.click();
+//        waitABit(1000);
+        WebElementFacade opcionVerInformacionPoliza = esperarElemento(".//*[@id='SubmissionWizard:LOBWizardStepGroup:PolicyInfo']/div");
+        opcionVerInformacionPoliza.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().click();
     }
 
     public void seleccionarOrganizacionDeVentas(String organizacion) {
-        espera(listaOrganizacionDeVentas, 2);
-        listaOrganizacionDeVentas.clear();
+//        espera(listaOrganizacionDeVentas, 2);
+        WebElementFacade listaOrganizacionDeVentas = findBy(".//*[@id='SubmissionWizard:LOBWizardStepGroup:LineWizardStepSet:PAPolicyInfoScreen:SubmissionWizard_PolicyInfoDV:PolicyInfoInputSet:SalesOrganizationType-inputEl']");
+        listaOrganizacionDeVentas.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().clear();
         listaOrganizacionDeVentas.sendKeys(organizacion);
-        waitABit(2000);
+//        waitABit(2000);
         listaOrganizacionDeVentas.sendKeys(Keys.ENTER);
-        waitABit(2000);
+//        waitABit(2000);
     }
 
     public void validarTipoCanalVentas(ExamplesTable tipoCanal) throws Exception {
-        listaTipoCanalDeVenta.click();
+        listaTipoCanalDeVenta.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().click();
         this.validarDatosDeLaLista(tipoCanal);
     }
 
@@ -76,7 +81,7 @@ public class DisponibilidadDetalleProductoPage extends Guidewire {
         List<WebElementFacade> elementosTipoCanalVentas;
         List<String> elementosRequeridos = GwNavegacionUtil.obtenerTablaDeEjemplosDeUnaColumna(tipoCanal);
         for (String tipo : elementosRequeridos) {
-            elementosTipoCanalVentas = withTimeoutOf(1, TimeUnit.SECONDS).findAll("//li[contains(.,'"+tipo+"')]");
+            elementosTipoCanalVentas = withTimeoutOf(10, TimeUnit.SECONDS).findAll("//li[contains(.,'"+tipo+"')]");
             for (WebElementFacade lista : elementosTipoCanalVentas){
                 MatcherAssert.assertThat(tipo, Matchers.containsString(lista.getText()));
             }
@@ -85,45 +90,48 @@ public class DisponibilidadDetalleProductoPage extends Guidewire {
 
 
     public void seleccionarTipoCanalDeVentas(String tipoCanal) {
-        listaTipoCanalDeVenta.clear();
+        listaTipoCanalDeVenta.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().clear();
         listaTipoCanalDeVenta.sendKeys(tipoCanal);
         listaTipoCanalDeVenta.sendKeys(Keys.ENTER);
-        waitABit(2000);
+//        waitABit(2000);
     }
 
     public void validarPATipoPoliza(ExamplesTable paTipoPoliza) throws Exception {
-        listaPATipoPoliza.click();
+        listaPATipoPoliza.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().click();
         this.validarDatosDeLaLista(paTipoPoliza);
     }
 
     public void seleccionarPATipoPoliza(String tipoPoliza) {
-        listaPATipoPoliza.clear();
+        listaPATipoPoliza.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent().clear();
         listaPATipoPoliza.sendKeys(tipoPoliza);
         listaPATipoPoliza.sendKeys(Keys.ENTER);
-        waitABit(2000);
+//        waitABit(2000);
     }
 
     public void validarActualizacionDeListaTipoCanal() {
+        listaTipoCanalDeVenta.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent();
         MatcherAssert.assertThat(listaTipoCanalDeVenta.getText(), containsText(""));
     }
 
     public void validarActualizacionDeListaPATipoPoliza() {
+        listaPATipoPoliza.withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent();
         MatcherAssert.assertThat(listaPATipoPoliza.getText(), containsText(""));
     }
 
     public void irAVerInformacionDePoliza() {
-        waitABit(2000);
+//        waitABit(2000);
         this.seleccionarAgente("DIRECTO");
-        waitABit(3000);
+        waitForTextToAppear("Productos ofrecidos", 30000);
         String xpathBotonElegirProducto = BTN_ELEGIR_PRODUCTO_ + this.encontrarProducto().toString() + ":addSubmission']";
-        WebElementFacade botonElegirProducto = findBy(xpathBotonElegirProducto);
+        WebElementFacade botonElegirProducto = esperarElemento(xpathBotonElegirProducto);
         botonElegirProducto.click();
         /*waitFor(botonAsegurados);
         botonAsegurados.click();*/
     }
 
     public void seleccionarAgente(String nombreAgente) {
-        element(campoNombreAgente).waitUntilClickable();
+        element(campoNombreAgente).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilPresent();
+        element(campoNombreAgente).withTimeoutOf(15, TimeUnit.SECONDS).waitUntilClickable();
         element(campoNombreAgente).click();
         List<WebElementFacade> listaNombresAgentesElement = findAll(By.xpath(".//li[@role='option']"));
         if (!listaNombresAgentesElement.isEmpty()) {
@@ -137,7 +145,7 @@ public class DisponibilidadDetalleProductoPage extends Guidewire {
     }
 
     public Integer encontrarProducto(){
-        tablaProductos.waitUntilVisible();
+        tablaProductos.withTimeoutOf(10, TimeUnit.SECONDS).waitUntilVisible();
         Integer filaBoton = 0;
         List<WebElement> filas = tablaProductos.findElements(By.tagName("tr"));
         for (WebElement row : filas) {
@@ -148,5 +156,20 @@ public class DisponibilidadDetalleProductoPage extends Guidewire {
             filaBoton++;
         }
         return filaBoton;
+    }
+
+    public WebElementFacade esperarElemento(final String xpath) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(getDriver())
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElementFacade elemento = wait.until(new Function<WebDriver, WebElementFacade>() {
+            public WebElementFacade apply(WebDriver driver) {
+                return findBy(xpath);
+            }
+        });
+
+        return  elemento;
     }
 }
