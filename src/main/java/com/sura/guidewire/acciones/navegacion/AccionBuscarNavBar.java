6a6415086e2523da2b6dbe14a544563dd4e235bb
Andrés Guerrero <andresf.alarcon@gmail.com>
2016-07-11 16:30:40 -0500

    Retro

---
+++ src/main/java/com/sura/guidewire/acciones/navegacion/AccionBuscarNavBar.java
@@ -0,0 +1,28 @@
+package com.sura.guidewire.acciones.navegacion;
+
+import com.sura.policycenter.pages.policy.BusquedaDeCuentasPage;
+import net.serenitybdd.core.pages.PageObject;
+
+public class AccionBuscarNavBar extends PageObject{
+    private static String MENU_BUSCAR = "//a[contains(@id,'TabBar:SearchTab-btnInnerEl')]";
+    private static String OPCION_CUENTAS = "//a[contains(.,'Cuentas')]";
+
+
+    public AccionBuscarNavBar en() {
+        return this;
+    }
+
+    public AccionBuscarNavBar buscar() {
+        findBy(MENU_BUSCAR).click();
+        return this;
+    }
+
+    public BusquedaDeCuentasPage cuenta() {
+        findBy(OPCION_CUENTAS).click();
+        return switchToPage(BusquedaDeCuentasPage.class);
+    }
+
+
+
+
+}
