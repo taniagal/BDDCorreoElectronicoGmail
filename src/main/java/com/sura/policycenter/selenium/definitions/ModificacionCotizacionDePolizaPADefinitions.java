e8670ef04d88bacb7368f4722ef0eefc5997bcf8
Jhon Jevis Vergara Baza <jhon.vergara@ceiba.com.co>
2016-07-26 11:25:18 -0500

    Tarifa modificacion pa.

---
+++ src/main/java/com/sura/policycenter/selenium/definitions/ModificacionCotizacionDePolizaPADefinitions.java
@@ -0,0 +1,103 @@
+package com.sura.policycenter.selenium.definitions;
+
+
+import com.google.inject.name.Named;
+import com.sura.policycenter.selenium.pages.InicioPage;
+import com.sura.policycenter.selenium.steps.ModificacionCotizacionDePolizaSteps;
+import net.thucydides.core.annotations.Manual;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModificacionCotizacionDePolizaPADefinitions {
+
+    private InicioPage inicioPage() {
+        return ThucydidesWebDriverSupport.getPages().currentPageAt(InicioPage.class);
+    }
+
+    private final Map<String, String> infoCotizacionPoliza = new HashMap<>();
+
+    @Steps
+    ModificacionCotizacionDePolizaSteps cotizacionDePolizaSteps;
+
+    public ModificacionCotizacionDePolizaPADefinitions(){
+        infoCotizacionPoliza.put("numeroPoliza", "Número de póliza");
+        infoCotizacionPoliza.put("numeroCotizacion", "Número de cotización");
+        infoCotizacionPoliza.put("vigenciaPoliza", "Vigencia de la póliza");
+        infoCotizacionPoliza.put("tomador", "Tomador");
+        infoCotizacionPoliza.put("tipoDocumento", "Tipo documento");
+        infoCotizacionPoliza.put("numeroDocumento", "Número documento");
+        infoCotizacionPoliza.put("direccion", "Dirección");
+        infoCotizacionPoliza.put("tipoDireccion", "Tipo de dirección");
+        infoCotizacionPoliza.put("descripcionDireccion", "Descripción de la dirección");
+        infoCotizacionPoliza.put("empresaAseguradora", "Empresa aseguradora");
+        infoCotizacionPoliza.put("primaTotal", "Prima total");
+        infoCotizacionPoliza.put("impuestos", "Impuestos y cargos extra");
+        infoCotizacionPoliza.put("costoTotal", "Costo total");
+
+        infoCotizacionPoliza.put("DC_labelVehiculo", "Vehículo 1");
+        infoCotizacionPoliza.put("DC_placa", "Placa");
+        infoCotizacionPoliza.put("DC_codigoFasecolda", "Código fasecolda");
+        infoCotizacionPoliza.put("DC_marca", "Marca");
+        infoCotizacionPoliza.put("DC_modelo", "Modelo");
+        infoCotizacionPoliza.put("DC_linea", "Línea");
+        infoCotizacionPoliza.put("DC_descripcion", "Descripción");
+        infoCotizacionPoliza.put("DC_prima", "Prima");
+        infoCotizacionPoliza.put("DC_subtotal", "Subtotal");
+        infoCotizacionPoliza.put("DC_subtotalPrimas", "Subtotal de primas");
+        infoCotizacionPoliza.put("DC_impuesto", "Impuesto");
+
+
+    }
+
+    @Given("se ha realizado la cotizacion de la modificacion <cotizacion>")
+    public void irALaCotizacion(@Named("cotizacion") String cotizacion) {
+        inicioPage().irABuscarSubPoliza(cotizacion);
+    }
+
+    @When("ingrese al detalle de la modificacion")
+    public void verDetalleCotizacion() {
+        cotizacionDePolizaSteps.verDetalleCotizacion();
+    }
+
+    @Then("debo ver de la cotizacion la siguiente informacion $informacionCotizacion")
+    public void validarInformacionCotizacion(ExamplesTable informacionCotizacion) {
+        cotizacionDePolizaSteps.validarInformacionCotizacion(infoCotizacionPoliza, informacionCotizacion);
+    }
+
+    @Then("debo poder ver la direccion del tomador de la modificacion <direccion>, la cual se indico como principal")
+    public void validarDireccion(@Named("direccion") String direccion){
+        cotizacionDePolizaSteps.validarDireccionTomador(direccion);
+    }
+
+    @Then("se debe mostrar la informacion del detalle de modificacion por riesgo")
+    public void thenMostrarInformacionDetalleCotizacion(){
+        cotizacionDePolizaSteps.validarInformacionDetalleCotizacion(infoCotizacionPoliza);
+    }
+
+    @Then("se debe mostrar en la columna \"Termino\" de la modificacion el limite o deducible de la cobertura en el caso de que aplique")
+    public void thenValidarTerminoCobertura(){
+        cotizacionDePolizaSteps.validarTerminoCobertura();
+    }
+
+    @Then("se debe mostrar en la columna \"Termino\" de la modificacion el limite de la cobertura en el caso de que aplique")
+    public void thenValidarLimiteCobertura(){
+        cotizacionDePolizaSteps.validarTerminoCobertura();
+    }
+
+    @Then("se debe mostrar en la columna \"Termino\" de la modificacion el deducible de la cobertura en el caso de que aplique")
+    public void thenValidarDeducibleCobertura(){
+        cotizacionDePolizaSteps.validarTerminoCobertura();
+    }
+
+    @Then("debo ver los detalles del cambio del costo de la modificacion")
+    public void thenValidarDetallesCosto(){
+        cotizacionDePolizaSteps.validarDetallesCosto(infoCotizacionPoliza);
+    }
+}
--- src/test/resources/stories/policycenter/modificacion_cotizacion_de_poliza_pa.story
+++ src/test/resources/stories/policycenter/modificacion_cotizacion_de_poliza_pa.story
@@ -1,4 +1,4 @@
-Cotizacion De Poliza
+Modificacion Cotizacion De Poliza
 
 Meta:
 
@@ -11,12 +11,66 @@ en el proceso de modificacion de una poliza existente.
 
 Scenario: Informacion general de cotizacion
 GivenStories: stories/policycenter/login_policy.story
-Given he realizado la cotizacion de la modificacion <cotizacion>
-When ingrese al detalle de la cotizacion
-Then debo ver la siguiente informacion
-|numeroCotizacion|tomador|tipoDocumento|numeroDocumento|direccion|tipoDireccion|descripcionDireccion|empresaAseguradora|prima|impuestos|total|
-|22222225|DORIAN EASTMOND PULGARIN|CEDULA DE CIUDADANIA|1234567891|CRA 65 # 48-162, LOUISVILLE, Estados Unidos|Vivienda|Created by the Address Builder with code 0|Acme Low Hazard Insurance|$44.00|$3.00|$47.00|
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then debo ver de la cotizacion la siguiente informacion
+|numeroPoliza|numeroCotizacion|tomador|tipoDocumento|numeroDocumento|direccion|tipoDireccion|descripcionDireccion|empresaAseguradora|prima|impuestos|total|
+|TEST_22222222|55555555|DORIAN EASTMOND PULGARIN|CEDULA DE CIUDADANIA|1234567891|CRA 65 # 48-162, LOUISVILLE, Estados Unidos|Vivienda|Created by the Address Builder with code 0|Acme Low Hazard Insurance|$44.00|$3.00|$47.00|
 
 Examples:
 |cotizacion|
-|22222225|
+|55555555|
+
+Scenario: Validar direccion del tomador de la poliza
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then debo poder ver la direccion del tomador de la modificacion <direccion>, la cual se indico como principal
+
+Examples:
+|cotizacion|direccion|
+|55555555  |CRA 65 # 48-162, LOUISVILLE, Estados Unidos |
+
+Scenario: Quote
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then se debe mostrar la informacion del detalle de modificacion por riesgo
+
+Examples:
+|cotizacion |
+|55555555   |
+
+Scenario: Terminos detalle cotizacion
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then se debe mostrar en la columna "Termino" de la modificacion el limite o deducible de la cobertura en el caso de que aplique
+
+Examples:
+|cotizacion |
+|55555555   |
+
+Scenario: Quote - Termino de la cobertura - Limite
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then se debe mostrar en la columna "Termino" de la modificacion el limite de la cobertura en el caso de que aplique
+
+Examples:
+|cotizacion |
+|55555555   |
+
+Scenario: Quote - Termino de la cobertura - Deducible
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then se debe mostrar en la columna "Termino" de la modificacion el deducible de la cobertura en el caso de que aplique
+
+Examples:
+|cotizacion |
+|55555555   |
+
+Scenario: Quote - Informacion detalle de cambio de costo
+Given se ha realizado la cotizacion de la modificacion <cotizacion>
+When ingrese al detalle de la modificacion
+Then debo ver los detalles del cambio del costo de la modificacion
+
+Examples:
+|cotizacion |
+|55555555   |
