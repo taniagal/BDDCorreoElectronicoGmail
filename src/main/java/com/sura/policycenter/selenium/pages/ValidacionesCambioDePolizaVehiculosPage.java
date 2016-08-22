7149bb4c8ebb102b092936e17c2e007d66d3641f
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-08-22 15:00:11 -0500

    Se realice ajuste al mensaje que se muestra al presionar el boton Expedir Poliza

--- src/main/java/com/sura/policycenter/selenium/pages/ValidacionesCambioDePolizaVehiculosPage.java
+++ src/main/java/com/sura/policycenter/selenium/pages/ValidacionesCambioDePolizaVehiculosPage.java
@@ -8,12 +8,15 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.slf4j.LoggerFactory;
 
 
 
 public class ValidacionesCambioDePolizaVehiculosPage extends PageObject{
+    Actions act = new Actions(getDriver());
     @FindBy(xpath = ".//td/div/div/div/a[6]/span/span/span/span")
     private WebElementFacade botonEmitirPoliza;
     @FindBy(xpath = ".//div[5]/div[3]/div/div/a/span/span/span")
@@ -31,9 +34,9 @@ public class ValidacionesCambioDePolizaVehiculosPage extends PageObject{
         commons.waitUntil(3000);
         botonEmitirPoliza.click();
         waitForTextToAppear("¿Está seguro de que desea expedir este cambio de póliza?");
-        withTimeoutOf(21, TimeUnit.SECONDS).waitFor(botonAceptarEmitirPoliza).shouldBePresent();
-        botonAceptarEmitirPoliza.click();
-        withTimeoutOf(28,TimeUnit.SECONDS).waitFor(botonAceptarEmitirPoliza).waitUntilNotVisible();
+        commons.waitUntil(1000);
+        act.sendKeys(Keys.ENTER).build().perform();
+        commons.waitUntil(1500);
     }
 
     public void validarMensaje(String mensaje){
