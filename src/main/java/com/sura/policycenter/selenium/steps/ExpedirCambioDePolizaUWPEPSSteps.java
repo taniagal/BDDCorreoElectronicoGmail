3b425f1778bb14e0c693ebf6d9fe64105f0d61b0
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-08-25 09:54:40 -0500

    Se inicia el diseno de las pruebas de aceptacion de la historia de UW PEP

---
+++ src/main/java/com/sura/policycenter/selenium/steps/ExpedirCambioDePolizaUWPEPSSteps.java
@@ -0,0 +1,7 @@
+package com.sura.policycenter.selenium.steps;
+
+/**
+ * Created by diegcaac on 24/08/2016.
+ */
+public class ExpedirCambioDePolizaUWPEPSSteps {
+}
---
+++ src/test/java/com/sura/policycenter/ExpedirCambioDePolizaUWPEPS.java
@@ -0,0 +1,7 @@
+package com.sura.policycenter;
+
+/**
+ * Created by diegcaac on 24/08/2016.
+ */
+public class ExpedirCambioDePolizaUWPEPS {
+}
