e111a554ddaa8a0c75421dba749717a625b188f4
Nelson Jhair Mosquera Palacios <nmosquera@qvision.com.co>
2016-08-26 16:14:51 -0500

    Waits -Se aumento el tiempo de espera para la seleccion de la organizacion al seleccionar un producto para cotizacion.

--- src/main/java/com/sura/policycenter/selenium/pages/DetallesDeUbicacionPage.java
+++ src/main/java/com/sura/policycenter/selenium/pages/DetallesDeUbicacionPage.java
@@ -113,6 +113,7 @@ public class DetallesDeUbicacionPage extends Commons {
         actions.sendKeys(Keys.ENTER).build().perform();
         selectItem(comboBoxOrganizacion,dato.get("organizacion"));
         waitForComboValue(comboBoxOrganizacionW,dato.get("organizacion"));
+        waitUntil(1000);
         selectItem(comboBoxCanal,dato.get("canal"));
         waitForComboValue(comboBoxCanalW,dato.get("canal"));
         seleccionarProducto(dato.get("producto"));
