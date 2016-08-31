1db183f03a6a574349f627b81d6d0779ad449bfe
Jonathan Mejia Leon <jmejia@qvision.com.co>
2016-08-31 15:45:26 -0500

    <Historia 2do esenario>
    Se realiza storyde segundo esenario

---
+++ src/main/java/com/sura/policycenter/selenium/pages/menu/opciones/poliza/InicioRenovacionPolizaPaPage.java
@@ -0,0 +1,114 @@
+package com.sura.policycenter.selenium.pages.menu.opciones.poliza;
+
+
+import com.sura.commons.selenium.Commons;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.hamcrest.MatcherAssert;
+import org.jbehave.core.model.ExamplesTable;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import javax.swing.*;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class InicioRenovacionPolizaPaPage extends Commons {
+
+
+    @FindBy(xpath = ".//*[@id='TabBar:PolicyTab']")
+    WebElementFacade menuPoliza;
+    @FindBy(xpath = ".//*[@id='TabBar:PolicyTab:PolicyTab_PolicyRetrievalItem-inputEl']")
+    WebElementFacade menuNumeropoliza;
+    @FindBy(xpath = ".//*[@id='messagebox-1001-displayfield-inputEl']")
+    WebElementFacade lblMensajeEnPantalla;
+    @FindBy(xpath = ".//*[@id='PolicyFile:PolicyFileMenuActions-btnInnerEl']")
+    WebElementFacade btnAcciones;
+    @FindBy(xpath = ".//*[@id='PolicyFile:PolicyFileMenuActions:PolicyFileMenuActions_NewWorkOrder:PolicyFileMenuActions_RenewPolicy-textEl']")
+    WebElementFacade btnRenovarPoliza;
+    @FindBy(xpath = ".//*[@id='button-1008-btnInnerEl']")
+    WebElementFacade btnCancelarRenovacion;
+    @FindBy(xpath = ".//*[@id='button-1005-btnInnerEl']")
+    WebElementFacade btnAceptarRenovacion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:Next-btnInnerEl']")
+    WebElementFacade btnSiguiente;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:AccountInfoInputSet:OfficialIDInputSet:OfficialIDDV_Input-inputEl']")
+    WebElementFacade datoCedeulaTomador;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:PolicyInfoProducerOfRecordInputSet:Producer-inputEl']")
+    WebElementFacade datoOficinaDeRadicacion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:PolicyInfoProducerOfRecordInputSet:ProducerCode-inputEl']")
+    WebElementFacade datoAgente;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:AccountInfoInputSet:Name-inputEl']")
+    WebElementFacade datoNombre;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PADriversScreen:PADriversPanelSet:DriversListDetailPanel:DriverDetailsCV:PolicyContactDetailsDV:OfficialIDInputSet:OfficialIDDV_Input-inputEl']")
+    WebElementFacade datoCedulaAsegurado;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PADriversScreen:PADriversPanelSet:DriversListDetailPanel:DriverDetailsCV:PolicyContactDetailsDV:PolicyContactRoleNameInputSet:GlobalPersonNameInputSet:FirstName-inputEl']")
+    WebElementFacade datoPrimerNombreAsegurado;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PADriversScreen:PADriversPanelSet:DriversListDetailPanel:DriverDetailsCV:PolicyContactDetailsDV:PolicyContactRoleNameInputSet:GlobalPersonNameInputSet:LastName-inputEl']")
+    WebElementFacade datoPrimerApellidoAsegurado;
+
+
+    Commons commons = new Commons(getDriver());
+
+    public InicioRenovacionPolizaPaPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+    public void irABuscarCotizacion(String cotizacion) {
+        waitFor(menuPoliza).waitUntilPresent();
+        menuPoliza.click();
+        waitForAnyTextToAppear("Cotización", "Buscar pólizas");
+        waitFor(menuPoliza).waitUntilPresent();
+        menuPoliza.click();
+        menuPoliza.sendKeys(Keys.ARROW_DOWN);
+        menuNumeropoliza.waitUntilPresent();
+        commons.ingresarDato(menuNumeropoliza, cotizacion);
+        menuNumeropoliza.sendKeys(Keys.ENTER);
+        waitForTextToAppear("Cotización");
+        waitForTextToAppear(cotizacion);
+    }
+
+    public void irARenovacion() {
+        withTimeoutOf(30, TimeUnit.SECONDS).waitFor(btnAcciones).waitUntilPresent();
+        btnAcciones.click();
+        btnRenovarPoliza.click();
+    }
+
+    public void cancelaOperacionRenovacion() {
+        btnCancelarRenovacion.click();
+    }
+
+    public void aceptaOperacionRenovacion() {
+        btnAceptarRenovacion.click();
+    }
+
+    public void clickBotonSiguinete() {
+        btnSiguiente.click();
+    }
+
+    public void validaMensajeEnPantalla(String mensaje) {
+        MatcherAssert.assertThat("El mensaje en pantalla no cumple con la info", lblMensajeEnPantalla.getText().equals(mensaje));
+    }
+
+    public void validacionesPantallaFormularios(ExamplesTable datosTomador) {
+        Map<String, String> datos = datosTomador.getRow(0);
+        if (datos.get("rol").equals("tomador")) {
+            MatcherAssert.assertThat("cedula del tomador no valida", datoCedeulaTomador.getText().equals(datos.get("cedulaTomador")));
+            MatcherAssert.assertThat("el nombre del tomador no es valido", datoNombre.getText().equals(datos.get("nombre")));
+            MatcherAssert.assertThat("el nombre de oficina de radicacion", datoOficinaDeRadicacion.getText().equals(datos.get("oficinaRadicacion")));
+            MatcherAssert.assertThat("el nombre de agente", datoAgente.getText().equals(datos.get("codAgente")));
+        }
+        clickBotonSiguinete();
+        if (datos.get("rol").equals("asegurado")) {
+            MatcherAssert.assertThat("cedula del Asegurado no valida", datoCedulaAsegurado.getText().equals(datos.get("cedulaAsegurado")));
+            MatcherAssert.assertThat("primer nombre no valido", datoPrimerNombreAsegurado.getText().equals(datos.get("pNombre")));
+            MatcherAssert.assertThat("primer apellido no valido", datoPrimerApellidoAsegurado.getText().equals(datos.get("pApellido")));
+        }
+        clickBotonSiguinete();
+        if (datos.get("rol").equals("vehiculo")) {
+            JOptionPane.showMessageDialog(null, "ok");
+        }
+
+    }
+}
---
+++ src/test/resources/stories/policycenter/inicio_renovacion_de_poliza_pa.story
@@ -0,0 +1,47 @@
+inicio renovacion de poliza mrc
+
+Meta:
+
+@issue #CDSEG-821
+@Automatizador: Jonathan Mejia Leon
+Sprint 6
+
+Narrative:
+Como usuario de policy center
+Se requiere renovar una poliza ya expedida
+entonces se deben realiza las validaciones de PEPS y riesgos consultables.
+
+Scenario: validar mensaje y cancelar el inicio del proceso de renovacion
+GivenStories: stories/policycenter/login_policy.story
+GivenStories: stories/policycenter/login_policy.story
+Given que es necesario renovar una <poliza> de autos
+When quiera realizar esta renovacion
+Then se deben mostrar un mensaje <mensaje>
+And se cancela el proceso de renovacion
+
+Examples:
+|poliza       |mensaje|
+|TEST_22266674|¿Esta seguro de que desea renovar esta póliza?|
+
+
+Scenario: datos de renovacion en informacion de poliza
+Given que es necesario renovar una <poliza> de autos
+When quiera realizar esta renovacion
+And quiera aceptar esta renovacion
+Then se debe validar los datos del tomador:
+|cedulaTomador|nombre    |oficinaRadicacion|codAgente                                  |rol    |
+|1077567451   |EVANS RENE|SURA             |Standard Code 789Internal Producer Code - 1|tomador|
+Then validar campos informativos de asegurado:
+|cedulaAsegurado|pNombre|pApellido|rol      |
+|1294567891     |VRALLAN|ESTIGUAR |asegurado|
+Then validar campos informativos de vehiculo:
+|placa|modelo|codFasecolda|claseVehiculo     |marca|linea|rol     |zona|tipoServicio|motor          |chasis          |valorAsegura|rol     |
+|TKC459|2016 |00601182    |Camperos y pickups|Mazda|MPV  |vehiculo|2   |Particular  |ENGINEEEEEEASDK|CHASIS83DSBNCLOA|10000000.00 |vehiculo|
+
+Examples:
+|poliza       |
+|TEST_22266674|
+
+
+
+
