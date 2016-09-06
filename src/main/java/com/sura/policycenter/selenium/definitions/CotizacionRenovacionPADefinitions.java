6092caf88a9f7a651e2757ce352c9a27236adf85
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-09-06 10:28:19 -0500

    Se termina el diseno de las pruebas de aceptacion correspondientes a la funcionalidad de cotizacion de renovacion para la linea de auto personal

--- src/main/java/com/sura/policycenter/selenium/definitions/CotizacionRenovacionPADefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/CotizacionRenovacionPADefinitions.java
@@ -6,6 +6,7 @@ import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 import org.jbehave.core.model.ExamplesTable;
 
 public class CotizacionRenovacionPADefinitions {
@@ -20,6 +21,11 @@ public class CotizacionRenovacionPADefinitions {
         cotizacionMRCSteps.ir_A_Buscar_Cotizacion_Poliza(cotizacion);
     }
 
+    @When("ingrese revisar la cotizacion")
+    public void ingresarACotizacion(){
+        cotizacionRenovacionPASteps.ver_Detalle_De_Cotizacion();
+    }
+
     @Then("se debe mostrar la informacion del detalle de cotizacion por riesgo, donde se muestre: $detalleCotizacion")
     public void validarDetalleCotizacion(ExamplesTable detalleCotizacion){
         cotizacionRenovacionPASteps.validar_Detalle_De_Cotizacion(detalleCotizacion);
@@ -28,5 +34,20 @@ public class CotizacionRenovacionPADefinitions {
     @Then("en el detalle por coberturas mostrar: $detalleCobertura")
     public void validarDetallePorCobertura(ExamplesTable detalleCobertura){
         cotizacionRenovacionPASteps.validar_Detalle_Por_Cobertura(detalleCobertura);
+    }
+
+    @Then("se debe ver la direccion del tomador de la poliza la cual se indico como principal $direccion")
+    public void validarDireccion(ExamplesTable direccion){
+        cotizacionRenovacionPASteps.validar_Direccion_Del_Tomador(direccion);
+    }
+
+    @Then("se debe mostrar en la columna Termino el limite o deducible de la cobertura en el caso de que aplique $valorTermino")
+    public void validarValorTermino(ExamplesTable valorTermino){
+        cotizacionRenovacionPASteps.validar_Valor_Termino_De_La_Cobertura(valorTermino);
+    }
+
+    @Then("se debe mostrar la informacion general de la poliza cotizada con los siguientes datos: $informacionGeneral")
+    public void validarInformacionGeneralRenovacion(ExamplesTable informacionGeneral){
+        cotizacionRenovacionPASteps.validar_Informacion_General_De_Renovacion(informacionGeneral);
     }
 }
--- src/main/java/com/sura/policycenter/selenium/pages/renovacion/CotizacionRenovacionPAPage.java
+++ src/main/java/com/sura/policycenter/selenium/pages/renovacion/CotizacionRenovacionPAPage.java
@@ -4,14 +4,44 @@ import com.sura.commons.selenium.Commons;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
 import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class CotizacionRenovacionPAPage extends Commons{
 
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:ttlBar']")
+    private WebElementFacade labelCotizacion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyNumber-inputEl']")
+    private WebElementFacade campoNumeroPoliza;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:JobNumber-inputEl']")
+    private WebElementFacade campoNumeroRenovacion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyPeriod-inputEl']")
+    private WebElementFacade campoVigenciaPoliza;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:Insured-inputEl']")
+    private WebElementFacade campoTomador;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:DocumentType-inputEl']")
+    private WebElementFacade campoTipoDocumento;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:DocumentNumber-inputEl']")
+    private WebElementFacade campoNumeroDocumento;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyAddress:PolicyAddressDisplayInputSet:globalAddressContainer:GlobalAddressInputSet:AddressSummary-inputEl']")
+    private WebElementFacade campoDireccion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyAddress:PolicyAddressDisplayInputSet:AddressType-inputEl']")
+    private WebElementFacade campoTipoDireccion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyAddress:PolicyAddressDisplayInputSet:AddressDescription-inputEl']")
+    private WebElementFacade campoDescripcionDireccion;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:UWCompany-inputEl']")
+    private WebElementFacade campoEmpresaAseguradora;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:TotalPremium-inputEl']")
+    private WebElementFacade campoPrimaTotal;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:Taxes-inputEl']")
+    private WebElementFacade campoImpuestos;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:TotalCost-inputEl']")
+    private WebElementFacade campoCostoTotal;
     @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:RatingCumulDetailsPanelSet:0:0:LicensePlate-inputEl']")
     private WebElementFacade campoPlaca;
     @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:RatingCumulDetailsPanelSet:0:0:FasecoldaCode-inputEl']")
@@ -22,9 +52,28 @@ public class CotizacionRenovacionPAPage extends Commons{
     private WebElementFacade campoModelo;
     @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:RatingCumulDetailsPanelSet:0:0:Line-inputEl']")
     private WebElementFacade campoLinea;
+    @FindBy(xpath = ".//td[2]/div/div[2]/div/table/tbody/tr/td/div")
+    private WebElementFacade campoCobertura;
+    @FindBy(xpath = ".//tr[2]/td[2]/div")
+    private WebElementFacade campoValorTermino;
+    @FindBy(xpath = ".//tr[3]/td[2]/div")
+    private WebElementFacade campoDeducible;
+    @FindBy(xpath = ".//div[2]/div/table/tbody/tr/td[3]/div")
+    private WebElementFacade campoPrima;
+    @FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:RatingCumulDetailsPanelSet:0:0:costLV:VehicleSubTotal']")
+    private WebElementFacade campoSubtotalSumaPrimas;
+    @FindBy(xpath = ".//div[2]/div/table/tbody/tr/td/div/table/tbody/tr[3]/td/div/table/tbody/tr/td/div/table/tbody/tr/td/div/table/tbody/tr/td[2]/div/div[2]/div/table/tbody/tr/td[2]/div")
+    private WebElementFacade campoSubtotalPrimas;
+    /*@FindBy(xpath = ".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:Quote_SummaryDV:PolicyAddress:PolicyAddressDisplayInputSet:globalAddressContainer:GlobalAddressInputSet:AddressSummary-inputEl']")
+    private WebElementFacade campoDireccion;*/
 
     public CotizacionRenovacionPAPage (WebDriver driver){
         super(driver);
+    }
+
+    public void verDetalleCotizacion() {
+        withTimeoutOf(20, TimeUnit.SECONDS).waitFor(labelCotizacion).shouldBeVisible();
+        MatcherAssert.assertThat(labelCotizacion.getText(), Is.is(Matchers.equalTo("Cotización")));
     }
 
     public void validarDetalleCotizacion(ExamplesTable detalleCotizacion) {
@@ -37,6 +86,39 @@ public class CotizacionRenovacionPAPage extends Commons{
     }
 
     public void validarDetallePorCobertura(ExamplesTable detalleCobertura) {
+        Map<String, String> cobertura = detalleCobertura.getRows().get(0);
+        MatcherAssert.assertThat(campoCobertura.getText(), Matchers.containsString(cobertura.get("cobertura")));
+        MatcherAssert.assertThat(campoValorTermino.getText(), Matchers.containsString(cobertura.get("terminoCobertura")));
+        MatcherAssert.assertThat(campoPrima.getText(), Matchers.containsString(cobertura.get("prima")));
+        MatcherAssert.assertThat(campoSubtotalSumaPrimas.getText(), Matchers.containsString(cobertura.get("subtotal")));
+        MatcherAssert.assertThat(campoSubtotalPrimas.getText(), Matchers.containsString(cobertura.get("subtotalPrimas")));
+    }
 
+    public void validarDireccionTomador(ExamplesTable direccion) {
+        Map<String, String> direccionTomador = direccion.getRows().get(0);
+        MatcherAssert.assertThat(campoDireccion.getText(), Is.is(Matchers.equalTo(direccionTomador.get("direccion"))));
+    }
+
+    public void validarValorTermino(ExamplesTable valorTermino) {
+        Map<String, String> terminoCobertura = valorTermino.getRows().get(0);
+        MatcherAssert.assertThat(campoValorTermino.getText(), Is.is(Matchers.equalTo(terminoCobertura.get("limite"))));
+        MatcherAssert.assertThat(campoDeducible.getText(), Is.is(Matchers.equalTo(terminoCobertura.get("deducible"))));
+    }
+
+    public void validarInformacionGeneralRenovacion(ExamplesTable informacionGeneral) {
+        Map<String, String> informacionRenovacion = informacionGeneral.getRows().get(0);
+        MatcherAssert.assertThat(campoNumeroPoliza.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("numeroPoliza"))));
+        MatcherAssert.assertThat(campoNumeroRenovacion.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("numeroRenovacion"))));
+        MatcherAssert.assertThat(campoVigenciaPoliza.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("vigenciaPoliza"))));
+        MatcherAssert.assertThat(campoTomador.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("tomador"))));
+        MatcherAssert.assertThat(campoTipoDocumento.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("tipoDocumento"))));
+        MatcherAssert.assertThat(campoNumeroDocumento.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("numeroDocumento"))));
+        MatcherAssert.assertThat(campoDireccion.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("direccion"))));
+        MatcherAssert.assertThat(campoTipoDireccion.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("tipoDireccion"))));
+        MatcherAssert.assertThat(campoDescripcionDireccion.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("descripcionDireccion"))));
+        MatcherAssert.assertThat(campoEmpresaAseguradora.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("empresaAseguradora"))));
+        MatcherAssert.assertThat(campoPrimaTotal.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("primaTotal"))));
+        MatcherAssert.assertThat(campoImpuestos.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("impuestos"))));
+        MatcherAssert.assertThat(campoCostoTotal.getText(), Is.is(Matchers.equalTo(informacionRenovacion.get("costoTotal"))));
     }
 }
