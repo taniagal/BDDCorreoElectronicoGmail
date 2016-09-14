1096eab07340244e5ed6acff9bee45b18f8c9e7b
Jonathan Mejia Leon <jmejia@qvision.com.co>
2016-09-14 09:16:48 -0500

    nuevo esenario

---
+++ src/main/java/com/sura/policycenter/selenium/steps/ReglaPreRenovacionDirPaSteps.java
@@ -0,0 +1,12 @@
+package com.sura.policycenter.selenium.steps;
+
+
+import net.thucydides.core.pages.Pages;
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class ReglaPreRenovacionDirPaSteps extends ScenarioSteps{
+    public ReglaPreRenovacionDirPaSteps (Pages pages){
+        super(pages)
+    }
+
+}
