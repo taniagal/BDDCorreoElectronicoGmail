c7868c86f7cd61419098a86a895cd72381c786b9
Andrés Guerrero <andresf.alarcon@gmail.com>
2016-09-22 10:09:03 -0500

    Creación de men u de navegación con WidgetObjet de serenity, redefinición de la forma en la que estamos navegando, aplicando Single Responsability, solución a la deuda tecnica de herencia mecanica y semantica para la clase padre GuidewirePage, GuidewireSteps, pendiente definir la clase vacia GuidewireLoginPage y que en ella se defina el cambio de Page hacia GuidewuirePage.

---
+++ src/main/java/com/sura/gw/navegacion/steps/MenuEquipoSteps.java
@@ -0,0 +1,29 @@
+package com.sura.gw.navegacion.steps;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class MenuEquipoSteps extends ScenarioSteps {
+
+    @Step
+    public void consultar_numero_de_subscripcion(String numSubscripcion) {
+        String xpathTxtNumeroSubscripcion = ".//input[contains(@name,'SubmissionNumberSearchItem')]";
+        getDriver().findElement(By.xpath(xpathTxtNumeroSubscripcion));
+        getDriver().switchTo().activeElement().sendKeys(Keys.ENTER);
+    }
+
+    @Step
+    public void consultar_numero_de_poliza(String numeroDepoliza) {
+        String xpathTxtNumeroPoliza = ".//input[contains(@name,'PolicyRetrievalItem')]";
+        getDriver().findElement(By.xpath(xpathTxtNumeroPoliza));
+        getDriver().switchTo().activeElement().sendKeys(Keys.ENTER);
+    }
+
+    @Step
+    public void nueva_cotizacion(){
+        String xpathBtnNuevaCotizacion = ".//a[contains(@id,'TabBar:PolicyTab:PolicyTab_NewSubmission-textEl')]";
+        getDriver().findElement(By.xpath(xpathBtnNuevaCotizacion)).click();
+    }
+}
---
+++ src/main/java/com/sura/gw/navegacion/widget/NavegacionSuperiorWidgetImpl.java
@@ -0,0 +1,48 @@
+package com.sura.gw.navegacion.widget;
+
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.serenitybdd.core.pages.WidgetObjectImpl;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+
+import java.util.concurrent.TimeUnit;
+
+public class NavegacionSuperiorWidgetImpl extends WidgetObjectImpl implements INavegacionSuperiorWidget {
+
+    private static final String XPATH_CONTENEDOR_SUBMENU_DESPLEGABLE = "//div[contains(@class, 'x-panel x-layer x-panel-default x-menu x-border-box')]";
+
+    public NavegacionSuperiorWidgetImpl(PageObject page, ElementLocator locator, long timeoutInMilliseconds) {
+        super(page, locator, timeoutInMilliseconds);
+    }
+
+    @Override
+    public void deplegarMenu(MenuNavegacionSuperiorEnum menu) {
+        getPage().setImplicitTimeout(1, TimeUnit.SECONDS);
+        if (esMenuVisible(menu)) {
+            WebElementFacade menuWE = getPage().moveTo(menu.xpath());
+            menuWE.setWindowFocus();
+            menuWE.sendKeys(Keys.ARROW_DOWN);
+            if (!esSubMenuDeMenuVisible()) {
+                getPage().shouldBeVisible(By.xpath(menu.xpath()));
+            }
+        }
+        getPage().resetImplicitTimeout();
+    }
+
+
+    private boolean esMenuVisible(MenuNavegacionSuperiorEnum menu) {
+        return getPage().isElementVisible(By.xpath(menu.xpath()));
+    }
+
+    private boolean esSubMenuDeMenuVisible() {
+        return getPage().isElementVisible(By.xpath(XPATH_CONTENEDOR_SUBMENU_DESPLEGABLE));
+    }
+
+
+    @Override
+    public void seleccionarMenu(MenuNavegacionSuperiorEnum menu) {
+        // TODO: 19/09/2016
+    }
+}
