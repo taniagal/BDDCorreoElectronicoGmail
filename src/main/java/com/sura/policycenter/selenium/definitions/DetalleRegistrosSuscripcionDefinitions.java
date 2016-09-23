0077e1015f9d0a6e6fc220e46182e78cae5b3ceb
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2016-09-23 10:48:58 -0500

    -borrado de imports -ajuste de waits

--- src/main/java/com/sura/policycenter/selenium/definitions/DetalleRegistrosSuscripcionDefinitions.java
+++ src/main/java/com/sura/policycenter/selenium/definitions/DetalleRegistrosSuscripcionDefinitions.java
@@ -1,12 +1,9 @@
 package com.sura.policycenter.selenium.definitions;
 
 import com.google.inject.name.Named;
-import com.sura.policycenter.selenium.pages.InicioPage;
 import com.sura.policycenter.selenium.steps.DetalleRegistrosSuscripcionSteps;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
-import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.WebDriver;
