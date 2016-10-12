413daf68de90f8ba3e04fb0c40852853239ddf86
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-10-12 07:52:27 -0500

    Se realiza diseno de las pruebas de aceptacion correspondientes a la funcionalidad de deseo de financiacion para la operacion de renovacion en la linea de auto personal

---
+++ src/main/java/com/sura/policycenter/selenium/pages/renovacion/RenovacionDeseoFinanciacionPaPage.java
@@ -0,0 +1,97 @@
+package com.sura.policycenter.selenium.pages.renovacion;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jbehave.core.model.ExamplesTable;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RenovacionDeseoFinanciacionPaPage extends PageObject{
+
+    Actions act = new Actions(getDriver());
+    @FindBy(xpath = ".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:_msgs']")
+    private WebElementFacade mensajeValidacion;
+    @FindBy(xpath = ".//*[@id='WebMessageWorksheet:WebMessageWorksheetScreen:grpMsgs']")
+    private WebElementFacade grupoMensajesValidacion;
+
+    public RenovacionDeseoFinanciacionPaPage(WebDriver driver){
+        super(driver);
+    }
+
+    public void seleccionarDeseoFinanciacionSi() {
+        WebElementFacade deseoFinanciacionSi = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:PolicyInfoInputSet:FundedPolicyInputSet:QuestionFundedPolicy_true-inputEl']");
+        withTimeoutOf(20, TimeUnit.SECONDS).waitFor(deseoFinanciacionSi).click();
+        WebElementFacade numeroCuotas = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:RenewalWizard_PolicyInfoDV:PolicyInfoInputSet:FundedPolicyInputSet:FundedPolicyQuotaNumber-inputEl']");
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(numeroCuotas).clear();
+        numeroCuotas.typeAndTab("12");
+    }
+
+    public void seleccionarOpcionSiguiente() {
+        WebElementFacade botonSiguiente = findBy(".//*[@id='RenewalWizard:Next-btnInnerEl']");
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(botonSiguiente).click();
+    }
+
+    public void validarMensajeDeseoFinanciacion(ExamplesTable mensaje) {
+        Map<String, String> mensajeFinanciacion = mensaje.getRows().get(0);
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(mensajeValidacion).shouldBeCurrentlyVisible();
+        MatcherAssert.assertThat(mensajeValidacion.getTextValue(), Matchers.containsString(mensajeFinanciacion.get("mensaje")));
+    }
+
+    public void irARevisionDePoliza(){
+        WebElementFacade revisionPoliza = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:PolicyReview']/div");
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(revisionPoliza).click();
+        waitABit(500);
+        revisionPoliza.click();
+    }
+
+    public void cotizarRenovacionPoliza() {
+        WebElementFacade cotizarRenovacion = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_DifferencesScreen:JobWizardToolbarButtonSet:RenewalQuote-btnInnerEl']");
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(cotizarRenovacion).click();
+    }
+
+    public void validarMensajeFinanciacionCotizacionRenovacion(ExamplesTable mensaje) {
+        Map<String, String> mensajeFinanciacion = mensaje.getRows().get(0);
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(grupoMensajesValidacion).shouldBeCurrentlyVisible();
+        MatcherAssert.assertThat(grupoMensajesValidacion.getTextValue(), Matchers.containsString(mensajeFinanciacion.get("mensaje")));
+    }
+
+    public void validarNumeroDeCuotas(ExamplesTable numeroCuotas) {
+        Map<String, String> cuotas = numeroCuotas.getRows().get(0);
+        WebElementFacade botonNumeroCuotas = findBy(".//tr[12]/td/table/tbody/tr/td[2]/table/tbody/tr/td[2]/div");
+        WebElementFacade cuota11 = findBy(".//li[contains(.,'11')]");
+        WebElementFacade cuota12 = findBy(".//li[contains(.,'12')]");
+        withTimeoutOf(20,TimeUnit.SECONDS).waitFor(botonNumeroCuotas).click();
+        MatcherAssert.assertThat(cuota11.getText(), Matchers.is(Matchers.equalTo(cuotas.get("cuota11"))));
+        MatcherAssert.assertThat(cuota12.getText(), Matchers.is(Matchers.equalTo(cuotas.get("cuota12"))));
+    }
+
+    public void validarCotizacionConIntencionDeFinanciacion() {
+        WebElementFacade labelCotizacion = findBy(".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:ttlBar']");
+        withTimeoutOf(30,TimeUnit.SECONDS).waitFor(labelCotizacion).shouldBeCurrentlyVisible();
+        MatcherAssert.assertThat(labelCotizacion.getText(), Matchers.is(Matchers.equalTo("Cotización")));
+    }
+
+    public void validarValorYNumeroDeCuotas(ExamplesTable detalleCotizacion) {
+        Map<String, String> detalle = detalleCotizacion.getRows().get(0);
+        WebElementFacade valorCuota = findBy(".//td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[2]/div/div[2]/div/table/tbody/tr/td[2]/div");
+        WebElementFacade numeroCuotas = findBy(".//td[2]/div/table/tbody/tr/td/div/table/tbody/tr/td[2]/div/div[2]/div/table/tbody/tr[2]/td[2]/div");
+        MatcherAssert.assertThat(valorCuota.getText(),Matchers.containsString(detalle.get("valorCuota")));
+        MatcherAssert.assertThat(numeroCuotas.getText(),Matchers.containsString(detalle.get("numeroCuota")));
+    }
+
+    public void expedirPolizaRenovacion() {
+        WebElementFacade botonOpcionesCompromiso = findBy(".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:JobWizardToolbarButtonSet:BindOptions-btnInnerEl']");
+        withTimeoutOf(30,TimeUnit.SECONDS).waitFor(botonOpcionesCompromiso).click();
+        WebElementFacade botonEmitir = findBy(".//*[@id='RenewalWizard:PostQuoteWizardStepSet:RenewalWizard_QuoteScreen:JobWizardToolbarButtonSet:BindOptions:IssueNow-textEl']");
+        withTimeoutOf(30,TimeUnit.SECONDS).waitFor(botonEmitir).click();
+        waitForTextToAppear("¿Está seguro de que desea emitir la renovación de la póliza?");
+        act.sendKeys(Keys.ENTER).build().perform();
+    }
+}
---
+++ src/test/resources/stories/policycenter/renovacion_deseo_financiacion_pa.story
@@ -0,0 +1,70 @@
+Renovacion Deseo Financiacion Pa
+
+Meta:
+
+@issue #CDSEG-2309
+@tag automator: diego_cardona_acevedo
+@Sprint 8
+
+Narrative:
+Como usuario de policy center  con el perfil  X
+Quiero poder realizar la cotizacion de la renovacion de una poliza de personal autos ya sea de forma manual o automatica
+
+Scenario: Validar mensaje de advertencia de financiacion - informacion de poliza
+GivenStories: stories/policycenter/login_policy.story
+Given estoy cotizando la renovacion de la poliza <cotizacion>
+And la poliza tiene intencion de financiacion
+When seleccione la opcion siguiente en la renovacion
+Then se debe mostrar un mensaje como advertencia
+|mensaje |
+|La financiación de la póliza está sujeta a aprobación por parte del área de financiación.|
+
+Examples:
+|cotizacion|
+|32112331  |
+
+Scenario: Validar numero de cuotas - informacion de poliza
+Given estoy cotizando la renovacion de la poliza <cotizacion>
+When la poliza tiene intencion de financiacion
+Then se debe mostrar el campo numero de cuotas, permitiendo seleccionar entre las opciones de 11 y 12
+|cuota11|cuota12|
+|11     |12     |
+
+Examples:
+|cotizacion|
+|32112331  |
+
+Scenario: Validar mensaje de advertencia de financiacion - cotizacion
+Given estoy cotizando la renovacion de la poliza <cotizacion>
+When realice la cotizacion de la renovacion con intencion de financiacion
+Then se debe mostrar una advertencia en la cotizacion
+|mensaje |
+|La financiación de la póliza está sujeta a aprobación por parte del área de financiación.|
+
+Examples:
+|cotizacion|
+|32112331  |
+
+Scenario: Validar que se muestre valor y numero de cuotas en la cotizacion
+Given estoy cotizando la renovacion de la poliza <cotizacion>
+When la cotizacion de renovacion tenga intencion de financiacion
+Then se debe mostrar en el detalle de la cotizacion el valor por cuota a pagar
+y el numero de cotas indicadas en la informacion de la poliza
+|valorCuota |numeroCuota|
+|$101.620   |11         |
+
+Examples:
+|cotizacion|
+|55556666  |
+
+Scenario: Validar mensaje de advertencia de financiacion - expedicion
+Given estoy cotizando la renovacion de la poliza <cotizacion>
+When la cotizacion de renovacion tenga intencion de financiacion
+And trate de expedir la poliza
+Then se debe mostrar una advertencia en la cotizacion
+|mensaje |
+|La financiación de la póliza está sujeta a aprobación por parte del área de financiación.|
+
+Examples:
+|cotizacion|
+|55556666  |
