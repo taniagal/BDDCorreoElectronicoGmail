3f24b328f1bd86696b2d8559345aacac480f0ac9
Nelson Jhair Mosquera Palacios <nmosquera@qvision.com.co>
2016-11-10 14:01:57 -0500

    Corrección -Cambio en el ingreso de la placa aleatoria para un vehiculo

--- src/main/java/com/sura/guidewire/policycenter/pages/ValidacionesInformacionDeVehiculoPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/ValidacionesInformacionDeVehiculoPage.java
@@ -2,20 +2,17 @@ package com.sura.guidewire.policycenter.pages;
 
 
 import com.sura.guidewire.policycenter.util.PageUtil;
-
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.thucydides.core.steps.StepInterceptor;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.hamcrest.core.Is;
 import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
@@ -66,8 +63,6 @@ public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
     @FindBy(xpath = ".//a[contains(.,'Descartar cambios no guardados')]")
     private WebElementFacade linkDescartarCambios;
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(StepInterceptor.class);
-
     protected static final int WAIT_TIME_28000 = 28000;
 
     public ValidacionesInformacionDeVehiculoPage(WebDriver driver) {
@@ -115,19 +110,9 @@ public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
         Map<String, String> vehiculo = datosVehiculo.getRow(0);
         waitUntil(WAIT_TIME_3000);
         selectItem(comboBoxPlan, vehiculo.get("plan"));
-        if (!"random".equals(vehiculo.get("placa"))) {
-            ingresarDato(campoTxtPlaca, vehiculo.get("placa"));
-        } else {
-            campoTxtPlaca.clear();
-            campoTxtPlaca.sendKeys("QWE" + (int) Math.floor(Math.random() * (100 - 999) + 999));
-        }
-        waitUntil(WAIT_TIME_1000);
+        ingresarPlaca(vehiculo);
         clickVehiculoServicio();
-        withTimeoutOf(WAIT_TIME_28, TimeUnit.SECONDS).waitFor(ExpectedConditions.textToBePresentInElement(tablaVehiculo, campoTxtPlaca.getText()));
-        waitUntil(WAIT_TIME_2000);
         seleccionarComboBoxModelo(vehiculo);
-        withTimeoutOf(WAIT_TIME_28, TimeUnit.SECONDS).waitFor(ExpectedConditions.textToBePresentInElement(tablaVehiculo, vehiculo.get("modelo")));
-        waitUntil(WAIT_TIME_2000);
         ingresarDato(campoTxtCodigoFasecolda, vehiculo.get("codigo_fasecolda"));
         campoTxtPlaca.click();
         waitForCampoTxtValorAsegurado(vehiculo);
@@ -139,6 +124,17 @@ public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
         selectItem(comboBoxVehiculoServicio, vehiculo.get("vehiculo_servicio"));
         agregarDescuento(vehiculo);
         MatcherAssert.assertThat("Error en el servicio de fasecolda", campoTxtValorAsegurado.getValue().contains(vehiculo.get("valor_asegurado")));
+    }
+
+    private void ingresarPlaca(Map<String, String> vehiculo) {
+        if (!"random".equals(vehiculo.get("placa"))) {
+            ingresarDato(campoTxtPlaca, vehiculo.get("placa"));
+        } else {
+            String placa = "QWE" + (int) Math.floor(Math.random() * (100 - 999) + 999);
+            campoTxtPlaca.clear();
+            ingresarDato(campoTxtPlaca, placa);
+        }
+        waitUntil(WAIT_TIME_1000);
     }
 
     private void waitForCampoTxtValorAsegurado(Map<String, String> vehiculo) {
@@ -156,6 +152,8 @@ public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
             LOGGER.info("StaleElementReferenceException at ValidacionesInformacionDeVehiculo Page 131 " + e);
             selectItem(comboBoxModelo, vehiculo.get("modelo"));
         }
+        withTimeoutOf(WAIT_TIME_28, TimeUnit.SECONDS).waitFor(ExpectedConditions.textToBePresentInElement(tablaVehiculo, vehiculo.get("modelo")));
+        waitUntil(WAIT_TIME_2000);
     }
 
     private void agregarDescuento(Map<String, String> vehiculo) {
@@ -187,6 +185,8 @@ public class ValidacionesInformacionDeVehiculoPage extends PageUtil {
             waitUntil(WAIT_TIME_2000);
             comboBoxVehiculoServicio.click();
         }
+        withTimeoutOf(WAIT_TIME_28, TimeUnit.SECONDS).waitFor(ExpectedConditions.textToBePresentInElement(tablaVehiculo, campoTxtPlaca.getText()));
+        waitUntil(WAIT_TIME_2000);
     }
 
     public void agregarCodigoFasecolda(String codigo) {
