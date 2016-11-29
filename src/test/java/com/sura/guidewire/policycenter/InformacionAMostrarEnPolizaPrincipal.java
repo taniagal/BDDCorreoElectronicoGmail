19a2eb6933c72553731d3e7535afb1621463f5c2
Juan Gabriel Zapata <jzapata@qvision.com.co>
2016-11-29 16:51:23 -0500

    Se comento el runner debido a que se termino la automaitzacion de la HU pero falta subir a dev por parte de configuracion.

--- src/test/java/com/sura/guidewire/policycenter/InformacionAMostrarEnPolizaPrincipal.java
+++ src/test/java/com/sura/guidewire/policycenter/InformacionAMostrarEnPolizaPrincipal.java
@@ -4,6 +4,8 @@ import net.serenitybdd.jbehave.SerenityStory;
 
 /**
  * Created by juanzaag on 24/11/2016.
+ * Quitar el comentado del runner apenas se suban los cambios a dev por parte de los configuradores
  */
-public class InformacionAMostrarEnPolizaPrincipal extends SerenityStory {
-}
+
+//public class InformacionAMostrarEnPolizaPrincipal extends SerenityStory {
+//}
