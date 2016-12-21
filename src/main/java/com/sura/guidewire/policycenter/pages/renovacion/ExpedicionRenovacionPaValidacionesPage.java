d3b661474fd2690a00762d575cd02295b7a118d3
Diego Cardona Acevedo <dcardona@qvision.com.co>
2016-12-21 07:43:29 -0500

    Se reaqlizan ajustes a la HU Expedicion Renovacion PA Validaciones

--- src/main/java/com/sura/guidewire/policycenter/pages/renovacion/ExpedicionRenovacionPaValidacionesPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/renovacion/ExpedicionRenovacionPaValidacionesPage.java
@@ -2,9 +2,16 @@ package com.sura.guidewire.policycenter.pages.renovacion;
 
 import com.sura.guidewire.policycenter.resources.PageUtil;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class ExpedicionRenovacionPaValidacionesPage extends PageUtil{
 
@@ -17,5 +24,52 @@ public class ExpedicionRenovacionPaValidacionesPage extends PageUtil{
     public void validarExpedicionDeRenovacion() {
         WebElementFacade botonAceptarPopup = findBy(".//span[contains(.,'Aceptar')]");
         botonAceptarPopup.waitUntilPresent().click();
+    }
+
+    public void validarAsuntosQueBloqueanExpedicion(ExamplesTable mensaje) {
+        waitForTextToAppear("Asuntos que bloquean la expedición");
+        WebElementFacade grupoAsuntosBloqueantes = findBy(".//*[@id='UWBlockProgressIssuesPopup:IssuesScreen:ApproveDV']");
+        Map<String, String> asuntosBloquantes;
+        grupoAsuntosBloqueantes.waitUntilPresent().waitUntilVisible();
+        for (int i = 0; i < mensaje.getRowCount(); i++) {
+            asuntosBloquantes = mensaje.getRows().get(i);
+            MatcherAssert.assertThat(grupoAsuntosBloqueantes.getText(), Matchers.containsString(asuntosBloquantes.get("mensaje")));
+        }
+    }
+
+    public void editarInformacionVehiculoRenovacion(ExamplesTable infoVehiculo) {
+        WebElementFacade validacionRetroactividad = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:RenewalWizard_PolicyInfoScreen:_msgs']/div");
+        WebElementFacade itemVehiculos = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:PersonalVehicles']/div/span");
+        setImplicitTimeout(WAIT_TIME_5,TimeUnit.SECONDS);
+        withTimeoutOf(WAIT_TIME_3, TimeUnit.SECONDS).waitFor(itemVehiculos).click();
+        if(validacionRetroactividad.isPresent()){
+            withTimeoutOf(WAIT_TIME_3, TimeUnit.SECONDS).waitFor(itemVehiculos).click();
+        }
+        resetImplicitTimeout();
+        WebElementFacade labelVehiculos = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PAVehiclesScreen:ttlBar']");
+        withTimeoutOf(WAIT_TIME_28, TimeUnit.SECONDS).waitFor(labelVehiculos).shouldBePresent();
+        WebElementFacade botonEditarTransaccion = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PAVehiclesScreen:JobWizardToolbarButtonSet:EditPolicy-btnInnerEl']");
+        setImplicitTimeout(WAIT_TIME_5,TimeUnit.SECONDS);
+        if(botonEditarTransaccion.isPresent()){
+            botonEditarTransaccion.click();
+            waitForTextToAppear("Si se edita esta transacción de la póliza, se invalida la cotización actual y podría ser necesaria una nueva aprobación de la aseguranza. ¿Está seguro de que desea continuar?");
+            actions.sendKeys(Keys.ENTER).build().perform();
+        }
+        resetImplicitTimeout();
+        Map<String, String> datosVehiculo = infoVehiculo.getRows().get(0);
+        WebElementFacade motor = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PAVehiclesScreen:PAVehiclesPanelSet:VehiclesListDetailPanel:VehiclesDetailsCV:PersonalAuto_VehicleDV:Engine_DV-inputEl']");
+        WebElementFacade chasis = findBy(".//*[@id='RenewalWizard:LOBWizardStepGroup:LineWizardStepSet:PAVehiclesScreen:PAVehiclesPanelSet:VehiclesListDetailPanel:VehiclesDetailsCV:PersonalAuto_VehicleDV:chasisl_DV-inputEl']");
+        if ("null".equals(datosVehiculo.get("motor")) && "null".equals(datosVehiculo.get("chasis"))){
+            withTimeoutOf(WAIT_TIME_5, TimeUnit.SECONDS).waitFor(motor).clear();
+            withTimeoutOf(WAIT_TIME_5, TimeUnit.SECONDS).waitFor(chasis).clear();
+        }else {
+            motor.sendKeys(datosVehiculo.get("motor"));
+            chasis.sendKeys(datosVehiculo.get("chasis"));
+        }
+    }
+
+    public void aceptarRenovacion() {
+        waitForTextToAppear("¿Está seguro de que desea emitir la renovación de la póliza?");
+        actions.sendKeys(Keys.ENTER).build().perform();
     }
 }
