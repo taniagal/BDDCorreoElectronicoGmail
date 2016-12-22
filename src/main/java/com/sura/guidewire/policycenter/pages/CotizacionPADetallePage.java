ba3d068091e98a4f469ab712a8427c2c03fd9996
Nelson Jhair Mosquera Palacios <nmosquera@qvision.com.co>
2016-12-22 16:07:42 -0500

    Correccion -Se repararon algunas pruebas que fallaban en jenkins por validacion de exclusividad y performance de la aplicacion.

--- src/main/java/com/sura/guidewire/policycenter/pages/CotizacionPADetallePage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/CotizacionPADetallePage.java
@@ -2,10 +2,6 @@ package com.sura.guidewire.policycenter.pages;
 
 
 import com.sura.guidewire.policycenter.resources.PageUtil;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.hamcrest.MatcherAssert;
@@ -14,7 +10,11 @@ import org.hamcrest.core.Is;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class CotizacionPADetallePage extends PageUtil {
@@ -42,14 +42,14 @@ public class CotizacionPADetallePage extends PageUtil {
         List<WebElement> allRows = tablaCoberturas.findElements(By.tagName("tr"));
         waitUntil(WAIT_TIME_1000);
         String validacion = null;
-        Map<String,String> terminoCoberturas = new HashMap<>();
+        Map<String, String> terminoCoberturas = new HashMap<>();
         try {
 
-            int i=0;
+            int i = 0;
             for (WebElement row : allRows) {
                 List<WebElement> cells = row.findElements(By.tagName("td"));
                 String terminoCob = cells.get(1).getText();
-                if(!"-".equals(terminoCob)) {
+                if (!"-".equals(terminoCob)) {
                     i++;
                     terminoCoberturas.put("dato" + i, terminoCob);
                 }
@@ -57,19 +57,19 @@ public class CotizacionPADetallePage extends PageUtil {
 
             waitFor(botonCoberturasPA).shouldBeVisible();
             botonCoberturasPA.click();
-            waitForTextToAppear("Coberturas de auto personal",WAIT_TIME_1000);
+            waitForTextToAppear("Coberturas de auto personal", WAIT_TIME_1000);
 
-            for(int j=1; j<=i; j++){
-                WebElementFacade coberturaAuto= findBy(".//div[contains(.,'"+terminoCoberturas.get("dato"+j)+"')]");
+            for (int j = 1; j <= i; j++) {
+                WebElementFacade coberturaAuto = findBy(".//div[contains(.,'" + terminoCoberturas.get("dato" + j) + "')]");
                 waitFor(coberturaAuto).shouldBeVisible();
                 MatcherAssert.assertThat(coberturaAuto.getText(), Is.is(Matchers.notNullValue()));
             }
 
             waitFor(botonCotizacion).shouldBeVisible();
             botonCotizacion.click();
-            waitForTextToAppear("Cotización",WAIT_TIME_1000);
+            waitForTextToAppear("Cotización", WAIT_TIME_1000);
 
-        }catch(Exception e) {
+        } catch (Exception e) {
             LOGGER.error("This is error", e);
             validacion = e.getMessage();
         }
--- src/test/resources/stories/policycenter/tarifacion/impuestos_de_tarifa.story
+++ src/test/resources/stories/policycenter/tarifacion/impuestos_de_tarifa.story
@@ -32,9 +32,9 @@ Examples:
 
 
 Scenario:  Verificar impuesto en la ciudad de circulación es San Andres
-Given estoy cotizando una poliza:
-|cuenta     |organizacion|producto               |canal            |
-|C1060447895|Sura        |Multiriesgo corporativo|Canal Tradicional|
+Given estoy cotizando una poliza de mrc:
+|organizacion|producto               |canal            |tipo_documento      |fecha_nacimiento|primer_nombre|primer_apellido|tipo_direccion         |direccion       |departamento|ciudad  |agente|
+|Sura        |Multiriesgo corporativo|Canal Tradicional|CEDULA DE CIUDADANIA|02/12/1990      |MIKASA        |AKERMAN       |DIRECCION DE RESIDENCIA|CALLE 54B #50-25|Antioquia   |Medellin|INT-3 |
 When agregue una nueva ubicacion departamento <departamento>, ciuad <ciudad>, direccion <direccion>
 And descripcion <descripcion>, actividad economica <actividad>
 When seleccione la cobertura:
