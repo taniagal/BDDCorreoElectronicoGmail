9427293e0e66585a98756f864c0c2216f1f79b3b
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-01-10 11:08:40 -0500

    reduccion de deuda tecnica del proyecto

--- src/test/java/com/sura/guidewire/policycenter/Sustituciones.java
+++ src/test/java/com/sura/guidewire/policycenter/Sustituciones.java
@@ -3,5 +3,5 @@ package com.sura.guidewire.policycenter;
 
 import net.serenitybdd.jbehave.SerenityStory;
 
-public class sustituciones extends SerenityStory {
+public class Sustituciones extends SerenityStory {
 }
