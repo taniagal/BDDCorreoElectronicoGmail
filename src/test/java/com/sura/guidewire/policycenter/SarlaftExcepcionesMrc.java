4e9fe71f309f37a1da0795e0c82c1544253243d4
SURAMERICANA\tanigral <tgrajales@qvision.com.co>
2017-01-10 16:36:15 -0500

    Se suspende temporalmente la HU Sarlaft Excepciones Mrc

--- src/test/java/com/sura/guidewire/policycenter/SarlaftExcepcionesMrc.java
+++ src/test/java/com/sura/guidewire/policycenter/SarlaftExcepcionesMrc.java
@@ -3,5 +3,5 @@ package com.sura.guidewire.policycenter;
 
 import net.serenitybdd.jbehave.SerenityStory;
 
-public class SarlaftExcepcionesMrc extends SerenityStory {
+public class SarlaftExcepcionesMrc {
 }
