b4fea7654dd899f58622afaee5e1e146a83d8def
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-01-13 13:17:22 -0500

    se ajusta la prueba para esperar el estado de un elemento.

--- src/main/java/com/sura/guidewire/policycenter/pages/TipoDeOnerosoPaPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/TipoDeOnerosoPaPage.java
@@ -5,6 +5,7 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import javax.swing.*;
 
@@ -33,6 +34,7 @@ public class TipoDeOnerosoPaPage extends PageUtil {
         clickElement(menuItemContacto1);
     }
     public void verificarMarca(){
+        waitFor(ExpectedConditions.attributeContains(radioBotonSeNecesitaCertificadoSi, "background-position", "0px -15px"));
         MatcherAssert.assertThat("Error no se encuentra marcado el campo Requiere certificado en Si","0px -15px".equals(radioBotonSeNecesitaCertificadoSi.getCssValue("background-position")));
     }
 }
