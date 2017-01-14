9acfe3914a2ae4f7615a86450f22618853b3f17d
Juan Gabriel Zapata <jzapata@qvision.com.co>
2017-01-13 21:23:21 -0500

    Refactura story:registro de coaseguro, al momento de  ingresar segunda aseguradora.

--- src/main/java/com/sura/guidewire/policycenter/pages/CoaseguroPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/CoaseguroPage.java
@@ -81,6 +81,7 @@ public class CoaseguroPage extends PageUtil {
                 MatcherAssert.assertThat("Sura debe estar por defecto en las aseguradoras", campoAseguradora.containsText("Sura"));
             } else {
                 act.click().build().perform();
+                waitUntil(WAIT_TIME_500);
                 act.sendKeys(aseguradora.getNombre()).build().perform();
                 act.sendKeys(Keys.TAB).build().perform();
                 waitUntil(WAIT_TIME_500);
