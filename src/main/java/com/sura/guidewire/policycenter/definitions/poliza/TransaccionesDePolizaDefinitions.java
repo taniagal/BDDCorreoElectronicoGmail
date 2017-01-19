ac2105980dd1f90549ba732f7d9c2dc88a0cc52c
Diego Cardona Acevedo <dcardona@qvision.com.co>
2017-01-19 14:09:49 -0500

    Se completa el diseno de las pruebas de aceptacion de la HU -Transacciones de Poliza

--- src/main/java/com/sura/guidewire/policycenter/definitions/poliza/TransaccionesDePolizaDefinitions.java
+++ src/main/java/com/sura/guidewire/policycenter/definitions/poliza/TransaccionesDePolizaDefinitions.java
@@ -1,10 +1,13 @@
 package com.sura.guidewire.policycenter.definitions.poliza;
 
+import com.sura.guidewire.policycenter.steps.ModificacionInformacionPolizaPASteps;
 import com.sura.guidewire.policycenter.steps.poliza.TransaccionesDePolizaSteps;
 import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Named;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
 
 
 public class TransaccionesDePolizaDefinitions {
@@ -12,14 +15,34 @@ public class TransaccionesDePolizaDefinitions {
     @Steps
     TransaccionesDePolizaSteps transaccionesDePolizaSteps;
 
+    @Steps
+    ModificacionInformacionPolizaPASteps modificacionInformacionPolizaPASteps;
+
+    @Given("se requiere comparar diferentes transacciones de una poliza <numeroPoliza>")
+    public void buscarPoliza(@Named("numeroPoliza") String numeroPoliza){
+        modificacionInformacionPolizaPASteps.ingresarPoliza(numeroPoliza);
+    }
+
+    @When("seleccione las transacciones a comparar")
+    public void seleccionarTransacciones() {
+        transaccionesDePolizaSteps.irATransaccionesDePoliza();
+        transaccionesDePolizaSteps.seleccionarTransacciones();
+        transaccionesDePolizaSteps.compararTransacciones();
+    }
+
     @When("consulte las transacciones de la poliza por el numero de transaccion <numeroTransaccion>")
     public void consultarTransaccionesDePolizaPorNumero(@Named("numeroTransaccion") String numeroTransaccion) {
         transaccionesDePolizaSteps.irATransaccionesDePoliza();
         transaccionesDePolizaSteps.consultarTransaccionesDePolizaPorNumero(numeroTransaccion);
     }
 
-    @Then("debo poder ver los detalles de esta transaccion")
-    public void thenDeboPoderVerLosDetallesDeEstaTransaccion() {
-        // PENDING
+    @Then("debo poder ver el detalle de esta transaccion")
+    public void visualizarDetalleTransaccion() {
+        transaccionesDePolizaSteps.validarDetalleTransaccion();
+    }
+
+    @Then("se deben poder visualizar las diferencias entre transacciones $diferenciasTransacciones")
+    public void visualizarDiferenciasTransacciones(ExamplesTable diferenciasTransacciones){
+        transaccionesDePolizaSteps.visualizarDiferenciasTransacciones(diferenciasTransacciones);
     }
 }
--- src/main/java/com/sura/guidewire/policycenter/steps/poliza/TransaccionesDePolizaSteps.java
+++ src/main/java/com/sura/guidewire/policycenter/steps/poliza/TransaccionesDePolizaSteps.java
@@ -1,18 +1,51 @@
 package com.sura.guidewire.policycenter.steps.poliza;
 
 import com.sura.guidewire.policycenter.pages.poliza.TransaccionesDePolizaPage;
+import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import org.fluentlenium.core.annotation.Page;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.Is;
+import org.jbehave.core.model.ExamplesTable;
+
+import java.util.Map;
 
 public class TransaccionesDePolizaSteps extends ScenarioSteps{
 
     @Page
     TransaccionesDePolizaPage transaccionesDePolizaPage;
 
+    @Step
     public void irATransaccionesDePoliza() {
         transaccionesDePolizaPage.irATransaccionesDePoliza();
     }
 
+    @Step
     public void consultarTransaccionesDePolizaPorNumero(String numeroTransaccion) {
+        transaccionesDePolizaPage.consultarTransaccionesDePolizaPorNumero(numeroTransaccion);
+    }
+
+    @Step
+    public void validarDetalleTransaccion() {
+        MatcherAssert.assertThat("El detalle de la transaccion no coincide con el esperado", transaccionesDePolizaPage.validarDetalleTransaccion(), Is.is(Matchers.equalTo(true)));
+    }
+
+    @Step
+    public void seleccionarTransacciones() {
+        transaccionesDePolizaPage.seleccionarTransacciones();
+    }
+
+    @Step
+    public void compararTransacciones() {
+        transaccionesDePolizaPage.compararTransacciones();
+    }
+
+    @Step
+    public void visualizarDiferenciasTransacciones(ExamplesTable diferenciasTransacciones) {
+        Map<String, String> diferencias = diferenciasTransacciones.getRows().get(0);
+        MatcherAssert.assertThat("La información de transacciones no coindice",
+                transaccionesDePolizaPage.validarDiferenciasTransacciones(diferencias.get("cotizacion"), diferencias.get("cambioPoliza")),
+                Is.is(Matchers.equalTo(true)));
     }
 }
