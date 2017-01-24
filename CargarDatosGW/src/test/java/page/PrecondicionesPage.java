b7f721475f266ba55d18e3fa8a60c6a924659c33
Nelson Jhair Mosquera Palacios <nmosquera@qvision.com.co>
2017-01-24 07:16:43 -0500

    Refcator -Se elimina import no usado de la librería org.slf4j

--- CargarDatosGW/src/test/java/page/PrecondicionesPage.java
+++ CargarDatosGW/src/test/java/page/PrecondicionesPage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.core.Is.is;
