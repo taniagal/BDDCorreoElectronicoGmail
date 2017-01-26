c15f5d3eab7bb41701c81dca58cfa101d5fb7c1f
SURAMERICANA\tanigral <tgrajales@qvision.com.co>
2017-01-26 11:11:46 -0500

    Se termina disenio de pruebas de aceptacion de la HU revision de cambio de poliza Pa

---
+++ src/main/java/com/sura/guidewire/policycenter/definitions/VisualizarCambiosEnPolizaDefintions.java
@@ -0,0 +1,37 @@
+package com.sura.guidewire.policycenter.definitions;
+
+import com.sura.guidewire.policycenter.steps.VisualizarCambiosEnPolizaSteps;
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class VisualizarCambiosEnPolizaDefintions {
+    @Steps
+    VisualizarCambiosEnPolizaSteps visualizarCambiosEnPolizaSteps;
+
+    @Then("debe existir la comparacion entre datos de poliza existente y poliza nueva")
+    public void ComparacionPolizas() {
+        visualizarCambiosEnPolizaSteps.comparacionDePolizas();
+    }
+
+    @When("cambie una cobertura <cobertura>")
+    public void cambiarCobertura(@Named("cobertura") String cobertura) {
+        visualizarCambiosEnPolizaSteps.cambiarCobertura(cobertura);
+    }
+
+    @Then("debe existir la comparacion entre existente y cambio de poliza <coberturaExistente> <CoberturaNueva>")
+    public void comparacionCoberturas(@Named("coberturaExistente") String coberturaExistente, @Named("coberturaNueva") String coberturaNueva) {
+        visualizarCambiosEnPolizaSteps.comparacionCoberturas(coberturaExistente, coberturaNueva);
+    }
+
+    @When("vaya a la opcion asegurados")
+    public void opcionAsegurados() {
+        visualizarCambiosEnPolizaSteps.opcionAsegurados();
+    }
+
+    @When("cambie la ciudad de circulacion <ciudad>")
+    public void cambiarCiudadCirculacion(@Named("ciudad") String ciudad) {
+        visualizarCambiosEnPolizaSteps.cambiarCiudadCirculacion(ciudad);
+    }
+}
