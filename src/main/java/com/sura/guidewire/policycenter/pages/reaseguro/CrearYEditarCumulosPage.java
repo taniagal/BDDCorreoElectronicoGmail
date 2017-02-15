b4032aaa8cf31b8f90456d84ff5b80b545d6f5ac
Jonathan Mejia <jomejia23@gmail.com>
2017-02-15 14:30:07 -0500

    se cubre exeception con metodo de esperar click

--- src/main/java/com/sura/guidewire/policycenter/pages/reaseguro/CrearYEditarCumulosPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/reaseguro/CrearYEditarCumulosPage.java
@@ -80,7 +80,7 @@ public class CrearYEditarCumulosPage extends PageUtil {
     public void ingresarDescripcionDeAcuerdoYDireccion(String descripcionDeAcuerdo) {
         actions.doubleClick(txtIngresaDescripcionAcuerdo).build().perform();
         actions.sendKeys(descripcionDeAcuerdo).build().perform();
-        btnAgregarDireccionRiesgoAplicable.click();
+        esperarYClickearBoton(btnAgregarDireccionRiesgoAplicable);
         listDireccionRiesgoAplicable.waitUntilPresent();
         esperarYClickearBoton(listDireccionRiesgoAplicable);
     }
