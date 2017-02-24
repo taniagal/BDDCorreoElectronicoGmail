beb866a98dfb8104bb82f822f0660011d21b02a0
Diego Cardona Acevedo <dcardona@qvision.com.co>
2017-02-24 09:57:18 -0500

    Disminucion Deuda Tecnica

--- src/main/java/com/sura/guidewire/policycenter/pages/poliza/BusquedaDePolizaPorCriteriosPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/poliza/BusquedaDePolizaPorCriteriosPage.java
@@ -27,8 +27,6 @@ public class BusquedaDePolizaPorCriteriosPage extends PageUtil {
     private WebElementFacade comboCriteriosBusqueda;
     @FindBy(xpath = ".//*[@id='PolicySearch:PolicySearchScreen:DatabasePolicySearchPanelSet:PolicySearchDV:CPSearch-inputEl']")
     private WebElementFacade campoCriterioDeBusqueda;
-    @FindBy(xpath = ".//*[@id='PolicySearch:PolicySearchScreen:DatabasePolicySearchPanelSet:PolicySearch_ResultsLV-body']")
-    private WebElementFacade filaResultados;
     public static final int TIEMPO_7 = 7000;
 
     public BusquedaDePolizaPorCriteriosPage(WebDriver driver) {
@@ -48,9 +46,7 @@ public class BusquedaDePolizaPorCriteriosPage extends PageUtil {
 
     public void buscarPorResultados(ExamplesTable aVerificar) {
         Map<String, String> datosVerificar = aVerificar.getRow(0);
-
         esperarHasta(TIEMPO_7);
-
         Set<String> llaves = datosVerificar.keySet();
         String contenidoTabla = tablaResultados.getText();
 
