cc7206ceee6606a3eda24eab85db45626db9c768
Jonathan Mejia Leon <jmejia@qvision.com.co>
2017-03-21 09:44:24 -0500

    se mejora deuda tecnica y se ingresa un wait para la lista de paises

--- src/main/java/com/sura/guidewire/policycenter/pages/reaseguro/CrearYEditarCumulosPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/reaseguro/CrearYEditarCumulosPage.java
@@ -21,10 +21,6 @@ public class CrearYEditarCumulosPage extends PageUtil {
     WebElementFacade btnCrearAcuerdoFacultativo;
     @FindBy(xpath = ".//*[@id='RIWorksheetPopup:Worksheet:RIWorksheetsPanelSet:0-body']/div/table/tbody/tr/td[2]/div")
     WebElementFacade txtIngresaDescripcionAcuerdo;
-    @FindBy(xpath = ".//*[@id='RIWorksheetPopup:Worksheet:RIWorksheetsPanelSet:RIWorksheetCV:Add-btnWrap']")
-    WebElementFacade btnAgregarDireccionRiesgoAplicable;
-    @FindBy(xpath = ".//*[@id='RIWorksheetPopup:Worksheet:RIWorksheetsPanelSet:RIWorksheetCV:Add:0:riskbutton']")
-    WebElementFacade listDireccionRiesgoAplicable;
     @FindBy(xpath = ".//*[@id='RIWorksheetPopup:Worksheet:RIWorksheetsPanelSet:RIWorksheetCV:worksheetItemsLV:WorksheetItemsLV_tb:Add-btnInnerEl']")
     WebElementFacade btnAgregaInformacionReaseguro;
     @FindBy(xpath = ".//*[@id='RIWorksheetPopup:Worksheet:RIWorksheetsPanelSet:RIWorksheetCV:worksheetItemsLV:WorksheetItemsLV:0:reName']")
@@ -62,8 +58,6 @@ public class CrearYEditarCumulosPage extends PageUtil {
     private static final double CONSTANTE_CIEN = 100.0;
     private static final int CONSTANTE_MIL = 1000;
     private double valorTasa = 0;
-    private double valorComisionReaseguroCedido = 0;
-    private double valorExpuesto = 0;
 
 
     public CrearYEditarCumulosPage(WebDriver driver) {
@@ -102,10 +96,11 @@ public class CrearYEditarCumulosPage extends PageUtil {
         try {
             linkNombreReasegurador.click();
         } catch (StaleElementReferenceException e) {
-            LOGGER.info("StaleElementReferenceException " + e);
+            LOGGER.error("StaleElementReferenceException " + e);
             esperarHasta(TIEMPO_1000);
             linkNombreReasegurador.click();
         }
+        listPaisSeleccionar.waitUntilPresent();
         seleccionarItem(listPaisSeleccionar, PAIS_ALEMANIA);
         seleccionarItem(listNombreReaseugurador, ASEGURADORA_ALLIANZ);
         clickearElemento(btnAceptarReasegurador);
@@ -151,7 +146,7 @@ public class CrearYEditarCumulosPage extends PageUtil {
 
     public String calculaTasaNetaDeCesionRegla() {
         listcomisionReaseguroCedido.waitUntilPresent();
-        valorComisionReaseguroCedido = Double.parseDouble(listcomisionReaseguroCedido.getText()) / CONSTANTE_CIEN;
+        double valorComisionReaseguroCedido = Double.parseDouble(listcomisionReaseguroCedido.getText()) / CONSTANTE_CIEN;
         valorTasa = Double.parseDouble($(VALOR).getText().replace(",", "."));
         double valorTasaBrutaDeCesion = valorTasa / (CONSTANTE_UNO - valorComisionReaseguroCedido);
         return Double.toString(valorTasaBrutaDeCesion).replace(".", ",");
@@ -164,7 +159,7 @@ public class CrearYEditarCumulosPage extends PageUtil {
             LOGGER.error("StaleElementReferenceException " + e);
         }
         String[] valorExpuestoCadena = listValorExpuestoRiesgo.getText().split(",");
-        valorExpuesto = Integer.parseInt(valorExpuestoCadena[0].substring(1).replaceAll("\\.", ""));
+        double valorExpuesto = Integer.parseInt(valorExpuestoCadena[0].substring(1).replaceAll("\\.", ""));
         valorTasa = Double.parseDouble($(VALOR).getText().replaceAll("\\.", ""));
         double valorPrimaBrutaDeCesion = (valorTasa / valorExpuesto) * CONSTANTE_MIL;
         return Double.toString(valorPrimaBrutaDeCesion).replace(".", ",");
