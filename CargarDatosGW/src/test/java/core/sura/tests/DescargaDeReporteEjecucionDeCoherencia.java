edf53cda76d43d0e7b06119d2e5d81a31fe176fc
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-03-22 16:53:18 -0500

    se agrega el import

--- CargarDatosGW/src/test/java/core/sura/tests/DescargaDeReporteEjecucionDeCoherencia.java
+++ CargarDatosGW/src/test/java/core/sura/tests/DescargaDeReporteEjecucionDeCoherencia.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import page.LoginPage;
+import page.DescargaDeReporteEjecucionDeCoherenciaPage;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
