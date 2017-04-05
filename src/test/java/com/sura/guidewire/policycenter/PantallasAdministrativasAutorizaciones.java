17b209c0766d18ff1519b6130fd2a307f6fe629c
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-04-05 15:23:45 -0500

    estabilizacion de pruebas

--- src/test/java/com/sura/guidewire/policycenter/PantallasAdministrativasAutorizaciones.java
+++ src/test/java/com/sura/guidewire/policycenter/PantallasAdministrativasAutorizaciones.java
@@ -2,8 +2,5 @@ package com.sura.guidewire.policycenter;
 
 import net.serenitybdd.jbehave.SerenityStory;
 
-/**
- * Created by tanigral on 17/03/2017.
- */
 public class PantallasAdministrativasAutorizaciones extends SerenityStory {
 }
