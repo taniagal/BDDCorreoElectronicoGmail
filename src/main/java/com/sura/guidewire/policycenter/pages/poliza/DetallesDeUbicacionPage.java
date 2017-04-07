a1a625ed8324ae1bca57ecce946c5d8e450eaeaf
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-04-07 10:42:50 -0500

    estabilizacion de pruebas

--- src/main/java/com/sura/guidewire/policycenter/pages/poliza/DetallesDeUbicacionPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/poliza/DetallesDeUbicacionPage.java
@@ -36,7 +36,7 @@ public class DetallesDeUbicacionPage extends PageUtil {
     private WebElementFacade labelNumeroDePisos;
     @FindBy(xpath = ".//*[@id='CPLocationPopup:LocationDetailDV:LocationDetailInputSet:BuildYear-labelEl']")
     private WebElementFacade labelAnioDeConstruccion;
-    @FindBy(css = ".message")
+    @FindBy(xpath = ".//*[@id='CPLocationPopup:_msgs']/div")
     private WebElementFacade divMensaje;
 
 
