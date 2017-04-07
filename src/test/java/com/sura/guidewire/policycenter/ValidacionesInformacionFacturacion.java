0aac9386f055d6d81a80017215d8e40e78e30acd
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-04-07 13:55:36 -0500

    se desctiva prueba porque necesita informacion de billing

--- src/test/java/com/sura/guidewire/policycenter/ValidacionesInformacionFacturacion.java
+++ src/test/java/com/sura/guidewire/policycenter/ValidacionesInformacionFacturacion.java
@@ -1,6 +1,5 @@
 package com.sura.guidewire.policycenter;
 
-import net.serenitybdd.jbehave.SerenityStory;
 
-public class ValidacionesInformacionFacturacion extends SerenityStory {
+public class ValidacionesInformacionFacturacion {
 }
