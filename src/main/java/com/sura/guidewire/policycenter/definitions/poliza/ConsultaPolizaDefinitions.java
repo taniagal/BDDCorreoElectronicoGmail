ab44e706f635d1bfcc66dec0cab564820860b822
Danny Andres Barrientos <dannybarrientos12@gmail.com>
2017-05-30 23:21:43 -0500

    CreacionDefinitions

---
+++ src/main/java/com/sura/guidewire/policycenter/definitions/poliza/ConsultaPolizaDefinitions.java
@@ -0,0 +1,12 @@
+package com.sura.guidewire.policycenter.definitions.poliza;
+
+
+import org.jbehave.core.annotations.Then;
+
+public class ConsultaPolizaDefinitions {
+
+    @Then("se validad la creacion de la poliza en SAP")
+    public void validarCreacionPolizaSaP(){
+
+    }
+}
