91142e2fee45f0d2f5fd9fedd12bb2ddfa4095ff
Eliana Alvarez Chaverra <ealvarez@qvision.com.co>
2017-06-06 13:18:55 -0500

    se deshabilitan pruebas de reaseguro por inestabilidad

--- src/test/java/com/sura/guidewire/policycenter/lote4/PolizasMrcCumuloSinFacultativo.java
+++ src/test/java/com/sura/guidewire/policycenter/lote4/PolizasMrcCumuloSinFacultativo.java
@@ -1,6 +1,4 @@
 package com.sura.guidewire.policycenter.lote4;
 
-import net.serenitybdd.jbehave.SerenityStory;
-
-public class PolizasMrcCumuloSinFacultativo extends SerenityStory {
+public class PolizasMrcCumuloSinFacultativo {
 }
