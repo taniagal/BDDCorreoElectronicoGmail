cd7af5f445a876eb62ce2db7457eed7386175520
Ferney Arroyave Quintero <farroyave@qvision.com.co>
2017-06-20 13:21:57 -0500

    Se inhabilita runner para estabilizar

--- src/test/java/com/sura/guidewire/policycenter/lote2/ReaseguroAutomaticoAutos.java
+++ src/test/java/com/sura/guidewire/policycenter/lote2/ReaseguroAutomaticoAutos.java
@@ -1,6 +1,4 @@
 package com.sura.guidewire.policycenter.lote2;
 
-import net.serenitybdd.jbehave.SerenityStory;
-
-public class ReaseguroAutomaticoAutos extends SerenityStory {
+public class ReaseguroAutomaticoAutos {
 }
