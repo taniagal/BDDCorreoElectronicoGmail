cb6c8927394969fcb6acadda637133e406006786
Ferney Arroyave Quintero <farroyave@qvision.com.co>
2017-07-21 11:17:26 -0500

    Se estabiliza método para iniciar cancelación de póliza

--- src/main/java/com/sura/guidewire/policycenter/pages/poliza/PolizaPage.java
+++ src/main/java/com/sura/guidewire/policycenter/pages/poliza/PolizaPage.java
@@ -71,6 +71,8 @@ public class PolizaPage extends PageUtil {
     private WebElementFacade botonEditarTransaccion;
     @FindBy(xpath = ".//span[contains(.,'Aceptar')]")
     private WebElementFacade botonAceptarTransaccion;
+    @FindBy(xpath = ".//*[@id='StartCancellation:StartCancellationScreen:NewCancellation-btnInnerEl']")
+    private WebElementFacade botonIniciarCancelacion;
 
     private String campoEmpleadoSura = ".//div[@id='PolicyFile_PolicyInfo:PolicyFile_PolicyInfoScreen:PolicyFile_PolicyInfoDV:PolicyInfoInputSet:PolicyEmployee_ExtInputSet:employee-inputEl']";
     private static String xpathMenuDesplegable = "//div[@class='x-boundlist x-boundlist-floating x-layer x-boundlist-default x-border-box']";
@@ -462,6 +464,10 @@ public class PolizaPage extends PageUtil {
     public void seleccionarOpcionRetirarTransaccion() {
         menuItemRetirarTransaccion.waitUntilPresent();
         clickearElemento(menuItemRetirarTransaccion);
+    }
+
+    public void clicIniciarCancelacion() {
+        clickearElemento(botonIniciarCancelacion);
     }
 
     public void confirmarCancelacion() {
