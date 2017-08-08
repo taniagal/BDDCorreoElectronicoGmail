0aae767fe220f41b6107a41469c83b0f2e67eecd
Juan Gabriel Zapata <jzapata@qvision.com.co>
2017-08-08 10:52:00 -0500

    Rebalanceo de lotes

--- src/test/java/com/sura/guidewire/policycenter/lote4/AdmonCotizacionCuentaDeclinar.java
+++ src/test/java/com/sura/guidewire/policycenter/lote4/AdmonCotizacionCuentaDeclinar.java
@@ -1,4 +1,4 @@
-package com.sura.guidewire.policycenter.lote2;
+package com.sura.guidewire.policycenter.lote4;
 
 import net.serenitybdd.jbehave.SerenityStory;
 
--- src/test/java/com/sura/guidewire/policycenter/lote7/CambioDePoliza.java
+++ src/test/java/com/sura/guidewire/policycenter/lote7/CambioDePoliza.java
@@ -1,4 +1,4 @@
-package com.sura.guidewire.policycenter.lote3;
+package com.sura.guidewire.policycenter.lote7;
 
 import net.serenitybdd.jbehave.SerenityStory;
 
