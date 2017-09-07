2db425ea8376e9e0ca51078c2fcb125707d03295
Ferney Arroyave Quintero <farroyave@qvision.com.co>
2017-09-07 11:12:09 -0500

    Reducción deuda técnica

--- src/main/java/com/sura/guidewire/policycenter/steps/renovacion/ExpedicionRenovacionPaValidacionesSteps.java
+++ src/main/java/com/sura/guidewire/policycenter/steps/renovacion/ExpedicionRenovacionPaValidacionesSteps.java
@@ -5,10 +5,12 @@ import com.sura.guidewire.policycenter.pages.renovacion.ExpedicionRenovacionPaVa
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
+import org.fluentlenium.core.annotation.Page;
 import org.jbehave.core.model.ExamplesTable;
 
 public class ExpedicionRenovacionPaValidacionesSteps extends ScenarioSteps{
 
+    @Page
     private ExpedicionRenovacionPaValidacionesPage expedicionRenovacionPaValidacionesPage;
 
     @Step
