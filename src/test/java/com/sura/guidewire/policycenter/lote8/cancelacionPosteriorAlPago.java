18c207fdac631b928980cf234b64b119da6e4404
UserQV <UserQV@PQVPC0BLFMD.suramericana.com.co>
2017-09-27 14:41:45 -0500

    Se automatiza escenario de cancelacion posterior al ultimo periodo pagado

--- src/test/java/com/sura/guidewire/policycenter/lote8/cancelacionPosteriorAlPago.java
+++ src/test/java/com/sura/guidewire/policycenter/lote8/cancelacionPosteriorAlPago.java
@@ -1,7 +1,13 @@
 package com.sura.guidewire.policycenter.lote8;
 
+import net.serenitybdd.jbehave.SerenityStory;
+
 /**
  * Created by UserQV on 26/09/2017.
  */
-public class cancelacionPosteriorAlPago {
+public class cancelacionPosteriorAlPago extends SerenityStory {
 }
+
+
+
+
