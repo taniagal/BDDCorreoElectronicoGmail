153ed7405ef36cb59551560def19f4e38eeaea58
SURAMERICANA\juanrert <jrestrepo@qvision.com.co>
2017-10-09 16:44:51 -0500

    Se hace refactor del modelo de autorizaciones

---
+++ src/test/java/com/sura/guidewire/policycenter/AutoPolizaNueva/PolizaNuevaModeloAutorizaciones.java
@@ -0,0 +1,9 @@
+package com.sura.guidewire.policycenter.lote8;
+
+import net.serenitybdd.jbehave.SerenityStory;
+
+/**
+ * Created by juanrert on 28/09/2017.
+ */
+public class PolizaNuevaModeloAutorizaciones extends SerenityStory {
+}
